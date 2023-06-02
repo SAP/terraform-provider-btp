75adc02a7a6a2e52c81fc186d5bdc0dfd181e6ad
Rui Nogueira <rui.nogueira@sap.com>
2023-06-02 15:10:11 +0200

    update directory user tests

--- internal/provider/datasource_directory_user_test.go
+++ internal/provider/datasource_directory_user_test.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -9,8 +10,8 @@ import (
 
 func TestDataSourceDirectoryUser(t *testing.T) {
 	t.Parallel()
-	t.Run("happy path", func(t *testing.T) {
-		rec := setupVCR(t, "fixtures/datasource_directory_user")
+	t.Run("happy path - default idp", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_directory_user.default_idp")
 		defer stopQuietly(rec)
 
 		resource.Test(t, resource.TestCase{
@@ -18,11 +19,11 @@ func TestDataSourceDirectoryUser(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProvider() + hclDatasourceDirectoryUser("uut", "05368777-4934-41e8-9f3c-6ec5f4d564b9", "jenny.doe@test.com", "sap.default"),
+					Config: hclProvider() + hclDatasourceDirectoryUserDefaultIdp("uut", "05368777-4934-41e8-9f3c-6ec5f4d564b9", "jenny.doe@test.com"),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "directory_id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
 						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "user_name", "jenny.doe@test.com"),
-						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "origin", "sap.default"),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "origin", "ldap"),
 						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "active", "false"),
 						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "family_name", ""),
 						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "given_name", ""),
@@ -34,89 +35,74 @@ func TestDataSourceDirectoryUser(t *testing.T) {
 			},
 		})
 	})
-	/*
-		t.Run("error path - name, role_template_name and app_id are mandatory", func(t *testing.T) {
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(nil),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProvider() + `data "btp_globalaccount_role" "uut" {}`,
-						ExpectError: regexp.MustCompile(`The argument "(name|role_template_name|app_id)" is required, but no definition was found.`),
-					},
-				},
-			})
-		})
 
-		t.Run("error path - name must not be empty", func(t *testing.T) {
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(nil),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProvider() + hclDatasourceGlobalaccountRole("uut", "", "b", "c"),
-						ExpectError: regexp.MustCompile(`Attribute name string length must be at least 1, got: 0`),
-					},
-				},
-			})
-		})
+	t.Run("happy path - custom idp", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_directory_user.custom_idp")
+		defer stopQuietly(rec)
 
-		t.Run("error path - role_template_name must not be empty", func(t *testing.T) {
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(nil),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProvider() + hclDatasourceGlobalaccountRole("uut", "a", "", "c"),
-						ExpectError: regexp.MustCompile(`Attribute role_template_name string length must be at least 1, got: 0`),
-					},
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclDatasourceDirectoryUserCustomIdp("uut", "05368777-4934-41e8-9f3c-6ec5f4d564b9", "jenny.doe@test.com", "terraformint"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "directory_id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "user_name", "jenny.doe@test.com"),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "origin", "terraformint"),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "active", "false"),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "family_name", ""),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "given_name", ""),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "id", ""),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "role_collections.#", "0"),
+						resource.TestCheckResourceAttr("data.btp_directory_user.uut", "verified", "false"),
+					),
 				},
-			})
+			},
 		})
+	})
 
-		t.Run("error path - app_id must not be empty", func(t *testing.T) {
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(nil),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProvider() + hclDatasourceGlobalaccountRole("uut", "a", "b", ""),
-						ExpectError: regexp.MustCompile(`Attribute app_id string length must be at least 1, got: 0`),
-					},
+	t.Run("error path - directory_id not a valid UUID", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceDirectoryUserDefaultIdp("uut", "this-is-not-a-uuid", "jenny.doe@test.com"),
+					ExpectError: regexp.MustCompile(`Attribute directory_id value must be a valid UUID, got: this-is-not-a-uuid`),
 				},
-			})
+			},
 		})
+	})
 
-		t.Run("error path - cli server returns error", func(t *testing.T) {
-			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				if strings.HasPrefix(r.URL.Path, "/login/") {
-					fmt.Fprintf(w, "{}")
-					return
-				}
-				w.WriteHeader(http.StatusNotFound)
-			}))
-			defer srv.Close()
-
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(srv.Client()),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceGlobalaccountRole("uut", "Global Account Viewer", "GlobalAccount_Viewer", "cis-local!b13"),
-						ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
-					},
+	t.Run("error path - directory_id, user_name and origin are mandatory", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + `data "btp_directory_user" "uut" {}`,
+					ExpectError: regexp.MustCompile(`The argument "(directory_id|user_name)" is required, but no definition was found.`),
 				},
-			})
+			},
 		})
-	*/
+	})
+
 }
 
-func hclDatasourceDirectoryUser(resourceName string, directoryId string, userName string, origin string) string {
+func hclDatasourceDirectoryUserCustomIdp(resourceName string, directoryId string, userName string, origin string) string {
 	template := `data "btp_directory_user" "%s" {
 	directory_id = "%s"
 	user_name 	 = "%s"
   	origin    	 = "%s"
 }`
-
 	return fmt.Sprintf(template, resourceName, directoryId, userName, origin)
+}
+
+func hclDatasourceDirectoryUserDefaultIdp(resourceName string, directoryId string, userName string) string {
+	template := `data "btp_directory_user" "%s" {
+	directory_id = "%s"
+	user_name 	 = "%s"
+}`
+	return fmt.Sprintf(template, resourceName, directoryId, userName)
 }
