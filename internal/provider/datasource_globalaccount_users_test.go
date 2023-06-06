83cf82b5a2028a0d308f50555bb22faf9f0605b6
Christian Volk <christian.volk@sap.com>
2023-06-06 17:30:16 +0200

    fix: issue #71

--- internal/provider/datasource_globalaccount_users_test.go
+++ internal/provider/datasource_globalaccount_users_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestDataSourceGlobalaccountUsers(t *testing.T) {
 	t.Parallel()
-	t.Run("happy path", func(t *testing.T) {
-		rec := setupVCR(t, "fixtures/datasource_globalaccount_users")
+	t.Run("happy path - default idp", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_globalaccount_users.default_idp")
 		defer stopQuietly(rec)
 
 		resource.Test(t, resource.TestCase{
@@ -18,17 +18,40 @@ func TestDataSourceGlobalaccountUsers(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProvider() + hclDatasourceGlobalaccountUsers("defaultidp", "sap.default"),
+					Config: hclProvider() + hclDatasourceGlobalaccountUsers("uut"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_globalaccount_users.defaultidp", "values.#", "6"),
+						resource.TestCheckResourceAttr("data.btp_globalaccount_users.uut", "values.#", "11"),
 					),
 				},
 			},
 		})
 	})
+	t.Run("happy path with custom idp", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_globalaccount_users.custom_idp")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclDatasourceGlobalaccountUsersWithCustomIdp("uut", "terraformint-platform"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.btp_globalaccount_users.uut", "values.#", "2"),
+					),
+				},
+			},
+		})
+	})
+	// TODO: error path with non existing idp
 }
 
-func hclDatasourceGlobalaccountUsers(resourceName string, origin string) string {
+func hclDatasourceGlobalaccountUsers(resourceName string) string {
+	template := `data "btp_globalaccount_users" "%s" {}`
+	return fmt.Sprintf(template, resourceName)
+}
+
+func hclDatasourceGlobalaccountUsersWithCustomIdp(resourceName string, origin string) string {
 	template := `data "btp_globalaccount_users" "%s" {
   origin    = "%s"
 }`
--- internal/provider/datasource_subaccount_users_test.go
+++ internal/provider/datasource_subaccount_users_test.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -18,38 +19,58 @@ func TestDataSourceSubaccountUsers(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProvider() + hclDatasourceSubaccountUsersDefaultIdp("defaultidp", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
+					Config: hclProvider() + hclDatasourceSubaccountUsersDefaultIdp("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_subaccount_users.defaultidp", "subaccount_id", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
-						resource.TestCheckResourceAttr("data.btp_subaccount_users.defaultidp", "values.#", "3"),
+						resource.TestCheckResourceAttr("data.btp_subaccount_users.uut", "subaccount_id", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
+						resource.TestCheckResourceAttr("data.btp_subaccount_users.uut", "values.#", "3"),
 					),
 				},
 			},
 		})
 	})
+	t.Run("happy path with custom idp", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_subaccount_users.custom_idp")
+		defer stopQuietly(rec)
 
-	/* TODO: make this code work. Currently we get the following error message: cannot unmarshal object into Go value of type []string */
-	/*
-		t.Run("happy path with custom idp", func(t *testing.T) {
-			rec := setupVCR(t, "fixtures/datasource_subaccount_users.custom_idp")
-			defer stopQuietly(rec)
-
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
-				Steps: []resource.TestStep{
-					{
-						Config: hclProvider() + hclDatasourceSubaccountUsersCustomIdp("mycustomidp", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "terraformint"),
-						Check: resource.ComposeAggregateTestCheckFunc(
-							resource.TestCheckResourceAttr("data.btp_subaccount_users.mycustomidp", "subaccount_id", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
-							resource.TestCheckResourceAttr("data.btp_subaccount_users.mycustomidp", "values.#", "3"),
-						),
-					},
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclDatasourceSubaccountUsersWithCustomIdp("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "terraformint-platform"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.btp_subaccount_users.uut", "subaccount_id", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
+						resource.TestCheckResourceAttr("data.btp_subaccount_users.uut", "values.#", "1"),
+					),
 				},
-			})
+			},
 		})
-	*/
-
+	})
+	t.Run("error path - subaccount_id mandatory", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + `data "btp_subaccount_users" "uut" {}`,
+					ExpectError: regexp.MustCompile(`The argument "subaccount_id" is required, but no definition was found`),
+				},
+			},
+		})
+	})
+	t.Run("error path - subaccount_id not a valid UUID", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceSubaccountUsersDefaultIdp("uut", "this-is-not-a-uuid"),
+					ExpectError: regexp.MustCompile(`Attribute subaccount_id value must be a valid UUID, got: this-is-not-a-uuid`),
+				},
+			},
+		})
+	})
+	// TODO: error path with non existing idp
 }
 
 func hclDatasourceSubaccountUsersDefaultIdp(resourceName string, subaccountId string) string {
@@ -59,12 +80,10 @@ func hclDatasourceSubaccountUsersDefaultIdp(resourceName string, subaccountId st
 	return fmt.Sprintf(template, resourceName, subaccountId)
 }
 
-/*
-func hclDatasourceSubaccountUsersCustomIdp(resourceName string, subaccountId string, origin string) string {
+func hclDatasourceSubaccountUsersWithCustomIdp(resourceName string, subaccountId string, origin string) string {
 	template := `data "btp_subaccount_users" "%s" {
 	subaccount_id = "%s"
 	origin = "%s"
 }`
 	return fmt.Sprintf(template, resourceName, subaccountId, origin)
 }
-*/
