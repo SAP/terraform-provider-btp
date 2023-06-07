b6c0f846afa6f11544f62ad7efcef6122f6b2d74
Christian Volk <christian.volk@sap.com>
2023-06-07 07:23:09 +0200

    fix: issue #71 (#80)

--- internal/provider/datasource_directory_users_test.go
+++ internal/provider/datasource_directory_users_test.go
@@ -19,16 +19,15 @@ func TestDataSourceDirectoryUsers(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProvider() + hclDatasourceDirectoryUsersDefaultIdp("defaultidp", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+					Config: hclProvider() + hclDatasourceDirectoryUsersDefaultIdp("uut", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_directory_users.defaultidp", "directory_id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
-						resource.TestCheckResourceAttr("data.btp_directory_users.defaultidp", "values.#", "2"),
+						resource.TestCheckResourceAttr("data.btp_directory_users.uut", "directory_id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+						resource.TestCheckResourceAttr("data.btp_directory_users.uut", "values.#", "2"),
 					),
 				},
 			},
 		})
 	})
-	/* TODO: find root cause for error message "cannot unmarshal object into Go value of type []string" (see https://github.com/SAP/terraform-provider-btp/issues/71)
 	t.Run("happy path with custom idp", func(t *testing.T) {
 		rec := setupVCR(t, "fixtures/datasource_directory_users.custom_idp")
 		defer stopQuietly(rec)
@@ -38,16 +37,15 @@ func TestDataSourceDirectoryUsers(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProvider() + hclDatasourceDirectoryUsersCustomIdp("mycustomidp", "05368777-4934-41e8-9f3c-6ec5f4d564b9", "terraformint"),
+					Config: hclProvider() + hclDatasourceDirectoryUsersWithCustomIdp("uut", "05368777-4934-41e8-9f3c-6ec5f4d564b9", "terraformint-platform"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_directory_users.mycustomidp", "directory_id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
-						resource.TestCheckResourceAttr("data.btp_directory_users.mycustomidp", "values.#", "2"),
+						resource.TestCheckResourceAttr("data.btp_directory_users.uut", "directory_id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+						resource.TestCheckResourceAttr("data.btp_directory_users.uut", "values.#", "1"),
 					),
 				},
 			},
 		})
 	})
-	*/
 	t.Run("error path - directory_id mandatory", func(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			IsUnitTest:               true,
@@ -72,7 +70,7 @@ func TestDataSourceDirectoryUsers(t *testing.T) {
 			},
 		})
 	})
-
+	// TODO: error path with non existing idp
 }
 
 func hclDatasourceDirectoryUsersDefaultIdp(resourceName string, directoryId string) string {
@@ -82,12 +80,10 @@ func hclDatasourceDirectoryUsersDefaultIdp(resourceName string, directoryId stri
 	return fmt.Sprintf(template, resourceName, directoryId)
 }
 
-/* TODO: find root cause for error message "cannot unmarshal object into Go value of type []string" (see https://github.com/SAP/terraform-provider-btp/issues/71)
-func hclDatasourceDirectoryUsersCustomIdp(resourceName string, directoryId string, origin string) string {
+func hclDatasourceDirectoryUsersWithCustomIdp(resourceName string, directoryId string, origin string) string {
 	template := `data "btp_directory_users" "%s" {
   directory_id    = "%s"
   origin    = "%s"
 }`
 	return fmt.Sprintf(template, resourceName, directoryId, origin)
 }
-*/
