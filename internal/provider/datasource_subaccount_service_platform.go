151e6a67384b2350d405ef548f16d3124965c72b
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-06-14 09:26:31 +0200

    fix: for 'error strings should not end with punctuation or newlines (ST1005)' (#141)

--- internal/provider/datasource_subaccount_service_platform.go
+++ internal/provider/datasource_subaccount_service_platform.go
@@ -128,7 +128,7 @@ func (ds *subaccountServicePlatformDataSource) Read(ctx context.Context, req dat
 	} else if !data.Name.IsNull() {
 		cliRes, _, err = ds.cli.Services.Platform.GetByName(ctx, data.SubaccountId.ValueString(), data.Name.ValueString())
 	} else {
-		err = fmt.Errorf("neither platform ID, nor platform Name have been provided.")
+		err = fmt.Errorf("neither platform ID, nor platform Name have been provided")
 	}
 
 	if err != nil {
