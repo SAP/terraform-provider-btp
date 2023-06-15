87d534ad749b72ddb4214fbb4911e13096e36580
Christian Volk <christian.volk@sap.com>
2023-06-15 12:54:03 +0200

    chore(globalaccount_users): test error cases

--- internal/provider/datasource_globalaccount_users_test.go
+++ internal/provider/datasource_globalaccount_users_test.go
@@ -2,6 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -26,7 +30,7 @@ func TestDataSourceGlobalaccountUsers(t *testing.T) {
 			},
 		})
 	})
-	t.Run("happy path with custom idp", func(t *testing.T) {
+	t.Run("happy path - with custom idp", func(t *testing.T) {
 		rec := setupVCR(t, "fixtures/datasource_globalaccount_users.custom_idp")
 		defer stopQuietly(rec)
 
@@ -44,6 +48,39 @@ func TestDataSourceGlobalaccountUsers(t *testing.T) {
 		})
 	})
 	// TODO: error path with non existing idp
+	t.Run("error path - origin must not be empty if given", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceGlobalaccountUsersWithCustomIdp("uut", ""),
+					ExpectError: regexp.MustCompile(`Attribute origin string length must be at least 1, got: 0`),
+				},
+			},
+		})
+	})
+	t.Run("error path - cli server returns error", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/login/") {
+				fmt.Fprintf(w, "{}")
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer srv.Close()
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(srv.Client()),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceGlobalaccountUsersWithCustomIdp("uut", "terraformint-platform"),
+					ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
+				},
+			},
+		})
+	})
 }
 
 func hclDatasourceGlobalaccountUsers(resourceName string) string {
