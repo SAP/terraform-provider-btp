b3bf080cc5eb21049a15a9654b2949102565fff6
Christian Volk <christian.volk@sap.com>
2023-06-15 15:43:30 +0200

    chore(subaccount_trust_configurations): test error cases

--- internal/provider/datasource_subaccount_trust_configurations_test.go
+++ internal/provider/datasource_subaccount_trust_configurations_test.go
@@ -2,7 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -27,7 +30,7 @@ func TestDataSourceSubaccountTrustConfigurations(t *testing.T) {
 			},
 		})
 	})
-	t.Run("happy path - subaccount not existing", func(t *testing.T) {
+	t.Run("error path - subaccount not existing", func(t *testing.T) {
 		rec := setupVCR(t, "fixtures/datasource_subaccount_trust_configurations.subaccount_not_existing")
 		defer stopQuietly(rec)
 
@@ -42,10 +45,53 @@ func TestDataSourceSubaccountTrustConfigurations(t *testing.T) {
 			},
 		})
 	})
+	t.Run("error path - subaccount_id not a valid UUID", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceSubaccountTrustConfigurations("uut", "this-is-not-a-uuid"),
+					ExpectError: regexp.MustCompile(`Attribute subaccount_id value must be a valid UUID, got: this-is-not-a-uuid`),
+				},
+			},
+		})
+	})
+	t.Run("error path - subaccount_id mandatory", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + `data "btp_subaccount_trust_configurations" "uut" {}`,
+					ExpectError: regexp.MustCompile(`The argument "subaccount_id" is required, but no definition was found`),
+				},
+			},
+		})
+	})
+	t.Run("error path - cli server returns error", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/login/") {
+				fmt.Fprintf(w, "{}")
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer srv.Close()
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(srv.Client()),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceSubaccountTrustConfigurations("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
+					ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
+				},
+			},
+		})
+	})
 }
 
 func hclDatasourceSubaccountTrustConfigurations(resourceName string, subaccountId string) string {
-	template := `data "btp_subaccount_trust_configurations" "%s" { subaccount_id = "%s" }`
-
-	return fmt.Sprintf(template, resourceName, subaccountId)
+	return fmt.Sprintf(`data "btp_subaccount_trust_configurations" "%s" { subaccount_id = "%s" }`, resourceName, subaccountId)
 }
