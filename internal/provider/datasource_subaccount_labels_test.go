006b3a285ddf657ce34eace08f55b0639991eced
Christian Volk <christian.volk@sap.com>
2023-06-15 17:12:22 +0200

    chore(subaccount_labels): test error cases (#172)

--- internal/provider/datasource_subaccount_labels_test.go
+++ internal/provider/datasource_subaccount_labels_test.go
@@ -2,6 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -27,56 +31,53 @@ func TestDataSourceSubaccountLabels(t *testing.T) {
 			},
 		})
 	})
-	/*
-		t.Run("error path - subaccount_id mandatory", func(t *testing.T) {
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(nil),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProvider() + `data "btp_subaccount_role_collections" "uut" {}`,
-						ExpectError: regexp.MustCompile(`The argument "subaccount_id" is required, but no definition was found`),
-					},
+	t.Run("error path - subaccount_id mandatory", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + `data "btp_subaccount_labels" "uut" {}`,
+					ExpectError: regexp.MustCompile(`The argument "subaccount_id" is required, but no definition was found`),
 				},
-			})
+			},
 		})
-		t.Run("error path - subaccount_id not a valid UUID", func(t *testing.T) {
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(nil),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProvider() + hclDatasourceSubaccountRoleCollection("uut", "this-is-not-a-uuid", "Subaccount Viewer"),
-						ExpectError: regexp.MustCompile(`Attribute subaccount_id value must be a valid UUID, got: this-is-not-a-uuid`),
-					},
+	})
+	t.Run("error path - subaccount_id not a valid UUID", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceSubaccountLabels("all", "this-is-not-a-uuid"),
+					ExpectError: regexp.MustCompile(`Attribute subaccount_id value must be a valid UUID, got: this-is-not-a-uuid`),
 				},
-			})
+			},
 		})
-		t.Run("error path - cli server returns error", func(t *testing.T) {
-			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				if strings.HasPrefix(r.URL.Path, "/login/") {
-					fmt.Fprintf(w, "{}")
-					return
-				}
-				w.WriteHeader(http.StatusNotFound)
-			}))
-			defer srv.Close()
+	})
+	t.Run("error path - cli server returns error", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/login/") {
+				fmt.Fprintf(w, "{}")
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer srv.Close()
 
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(srv.Client()),
-				Steps: []resource.TestStep{
-					{
-						Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceSubaccountRoleCollection("uut", "5357bda0-8651-4eab-a69d-12d282bc3247", "Subaccount Viewer"),
-						ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
-					},
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(srv.Client()),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceSubaccountLabels("all", "ef23ace8-6ade-4d78-9c1f-8df729548bbf"),
+					ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
 				},
-			})
-		})*/
+			},
+		})
+	})
 }
 
 func hclDatasourceSubaccountLabels(resourceName string, subaccountId string) string {
-	template := `data "btp_subaccount_labels" "%s" { subaccount_id = "%s" }`
-
-	return fmt.Sprintf(template, resourceName, subaccountId)
+	return fmt.Sprintf(`data "btp_subaccount_labels" "%s" { subaccount_id = "%s" }`, resourceName, subaccountId)
 }
