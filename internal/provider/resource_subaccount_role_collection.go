468c95ba8667222f179a74fd6b031ed5bc1fe834
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-06-15 09:56:41 +0200

    feature: test for resource_subaccount_role_collection

--- internal/provider/resource_subaccount_role_collection.go
+++ internal/provider/resource_subaccount_role_collection.go
@@ -26,6 +26,7 @@ type subaccountRoleCollectionRoleRefType struct {
 type subaccountRoleCollectionType struct {
 	SubaccountId types.String                          `tfsdk:"subaccount_id"`
 	Name         types.String                          `tfsdk:"name"`
+	Id           types.String                          `tfsdk:"id"`
 	Description  types.String                          `tfsdk:"description"`
 	Roles        []subaccountRoleCollectionRoleRefType `tfsdk:"roles"`
 }
@@ -60,12 +61,17 @@ __Further documentation:__
 					uuidvalidator.ValidUUID(),
 				},
 			},
+			"id": schema.StringAttribute{ // required hashicorps terraform plugin testing framework
+				DeprecationMessage:  "Use the `name` attribute instead",
+				MarkdownDescription: "The ID of the role collection.",
+				Computed:            true,
+			},
 			"name": schema.StringAttribute{
 				MarkdownDescription: "The name of the role collection.",
 				Required:            true,
 			},
 			"description": schema.StringAttribute{
-				MarkdownDescription: "Whether the role collection is readonly.",
+				MarkdownDescription: "The description of the role collection.",
 				Optional:            true,
 				Computed:            true,
 			},
@@ -148,6 +154,7 @@ func (rs *subaccountRoleCollectionResource) Create(ctx context.Context, req reso
 
 	plan.Name = types.StringValue(cliRes.Name)
 	plan.Description = types.StringValue(cliRes.Description)
+	plan.Id = types.StringValue(cliRes.Name)
 
 	diags = resp.State.Set(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
