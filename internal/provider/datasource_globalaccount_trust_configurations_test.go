a8c15f9eedf246faa2d9d7ed6adfe55888d241e2
Christian Volk <christian.volk@sap.com>
2023-06-15 15:32:09 +0200

    chore(globalaccount_trust_configurations): test error cases

--- internal/provider/datasource_globalaccount_trust_configurations_test.go
+++ internal/provider/datasource_globalaccount_trust_configurations_test.go
@@ -2,6 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -26,10 +30,29 @@ func TestDataSourceGlobalaccountTrustConfigurations(t *testing.T) {
 			},
 		})
 	})
+	t.Run("error path - cli server returns error", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/login/") {
+				fmt.Fprintf(w, "{}")
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer srv.Close()
 
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(srv.Client()),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceGlobalaccountTrustConfigurations("uut"),
+					ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
+				},
+			},
+		})
+	})
 }
 
 func hclDatasourceGlobalaccountTrustConfigurations(resourceName string) string {
-	template := `data "btp_globalaccount_trust_configurations" "%s" {}`
-	return fmt.Sprintf(template, resourceName)
+	return fmt.Sprintf(`data "btp_globalaccount_trust_configurations" "%s" {}`, resourceName)
 }
