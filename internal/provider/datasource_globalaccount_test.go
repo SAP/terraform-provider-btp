abc821233b0a4700d785d2b360ae66d0bd061e3d
Christian Volk <christian.volk@sap.com>
2023-06-15 14:00:45 +0200

    chore(globalaccount): test error cases (#159)

--- internal/provider/datasource_globalaccount_test.go
+++ internal/provider/datasource_globalaccount_test.go
@@ -2,6 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -30,6 +34,27 @@ func TestDataSourceGlobalAccount(t *testing.T) {
 						resource.TestCheckResourceAttr("data.btp_globalaccount.uut", "commercial_model", "Subscription"),
 						resource.TestCheckResourceAttr("data.btp_globalaccount.uut", "consumption_based", "true"),
 					),
+				},
+			},
+		})
+	})
+	t.Run("error path - cli server returns error", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/login/") {
+				fmt.Fprintf(w, "{}")
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer srv.Close()
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(srv.Client()),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceGlobalAccount("uut"),
+					ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
 				},
 			},
 		})
