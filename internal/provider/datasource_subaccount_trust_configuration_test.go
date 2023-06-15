29f5a23088e6c24cadd83b903624c1d78bfc5231
Christian Volk <christian.volk@sap.com>
2023-06-15 15:46:00 +0200

    chore(subaccount_trust_configuration): test error cases (#170)

--- internal/provider/datasource_subaccount_trust_configuration_test.go
+++ internal/provider/datasource_subaccount_trust_configuration_test.go
@@ -2,6 +2,10 @@ package provider
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -9,7 +13,7 @@ import (
 
 func TestDataSourceSubaccountTrustConfiguration(t *testing.T) {
 	t.Parallel()
-	t.Run("happy path", func(t *testing.T) {
+	t.Run("happy path - with default idp", func(t *testing.T) {
 		rec := setupVCR(t, "fixtures/datasource_subaccount_trust_configuration.default")
 		defer stopQuietly(rec)
 
@@ -33,7 +37,7 @@ func TestDataSourceSubaccountTrustConfiguration(t *testing.T) {
 			},
 		})
 	})
-	t.Run("happy path", func(t *testing.T) {
+	t.Run("happy path - with custom idp", func(t *testing.T) {
 		rec := setupVCR(t, "fixtures/datasource_subaccount_trust_configuration.custom_idp_exists")
 		defer stopQuietly(rec)
 
@@ -57,7 +61,8 @@ func TestDataSourceSubaccountTrustConfiguration(t *testing.T) {
 			},
 		})
 	})
-	t.Run("happy path", func(t *testing.T) {
+	// FIXME https://github.com/SAP/terraform-provider-btp/issues/167
+	/*t.Run("happy path", func(t *testing.T) {
 		rec := setupVCR(t, "fixtures/datasource_subaccount_trust_configuration.custom_idp_not_existing")
 		defer stopQuietly(rec)
 
@@ -77,6 +82,39 @@ func TestDataSourceSubaccountTrustConfiguration(t *testing.T) {
 						resource.TestCheckResourceAttr("data.btp_subaccount_trust_configuration.uut", "status", ""),
 						resource.TestCheckResourceAttr("data.btp_subaccount_trust_configuration.uut", "type", ""),
 					),
+				},
+			},
+		})
+	})*/
+	t.Run("error path - origin must not be empty", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceSubaccountTrustConfiguration("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", ""),
+					ExpectError: regexp.MustCompile(`Attribute origin string length must be at least 1, got: 0`),
+				},
+			},
+		})
+	})
+	t.Run("error path - cli server returns error", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/login/") {
+				fmt.Fprintf(w, "{}")
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer srv.Close()
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(srv.Client()),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProviderWithCLIServerURL(srv.URL) + hclDatasourceSubaccountTrustConfiguration("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "sap.default"),
+					ExpectError: regexp.MustCompile(`Received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
 				},
 			},
 		})
