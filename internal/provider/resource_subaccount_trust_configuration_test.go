bd9aea137428cab57feefb8b49d7319a08f3dae4
Rui Nogueira <rui.nogueira@sap.com>
2023-06-16 10:53:08 +0200

    finish minimal test

--- internal/provider/resource_subaccount_trust_configuration_test.go
+++ internal/provider/resource_subaccount_trust_configuration_test.go
@@ -10,8 +10,8 @@ import (
 func TestResourceSubaccountTrustConfiguration(t *testing.T) {
 	t.Parallel()
 	/*
-		t.Run("happy path - complete configuration 1", func(t *testing.T) {
-			rec := setupVCR(t, "fixtures/resource_subaccount_trust_configuration.complete1")
+		t.Run("happy path - complete configuration", func(t *testing.T) {
+			rec := setupVCR(t, "fixtures/resource_subaccount_trust_configuration.complete")
 			defer stopQuietly(rec)
 
 			resource.Test(t, resource.TestCase{
@@ -19,24 +19,25 @@ func TestResourceSubaccountTrustConfiguration(t *testing.T) {
 				ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 				Steps: []resource.TestStep{
 					{
-						Config: hclProvider() + hclResourceSubaccountTrustConfigurationComplete("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "terraformint.accounts400.ondemand.com", "terraform1-platform", "Custom Platform Identity Provider 1 for testing", "terraform1-tst-platform"),
+						Config: hclProvider() + hclResourceSubaccountTrustConfigurationComplete("uut", "0918a231-cb4c-4fc9-831f-d5db5dcab13c", "terraformint.accounts400.ondemand.com", "Custom IAS tenant number 2", "IAS tenant terraformint.accounts400.ondemand.com (OpenID Connect)", "temp-platform"),
 						Check: resource.ComposeAggregateTestCheckFunc(
 							resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "subaccount_id", regexpValidUUID),
 							resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "created_date", regexpValidRFC3999Format),
 							resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "last_modified", regexpValidRFC3999Format),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "id", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "identity_provider", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "name", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "description", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "origin", "hana-cloud-hana"),
+							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "id", "sap.custom"),
+							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "identity_provider", "terraformint.accounts400.ondemand.com"),
+							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "name", "Custom IAS tenant"),
+							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "description", "IAS tenant terraformint.accounts400.ondemand.com (OpenID Connect)"),
+							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "origin", "sap.custom"),
 						),
 					},
 				},
 			})
-		}) */
+		})
+	*/
 
-	t.Run("happy path - minimal configuration 1", func(t *testing.T) {
-		rec := setupVCR(t, "fixtures/resource_subaccount_trust_configuration.minimum1")
+	t.Run("happy path - minimal configuration", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/resource_subaccount_trust_configuration.minimal")
 		defer stopQuietly(rec)
 
 		resource.Test(t, resource.TestCase{
@@ -47,8 +48,8 @@ func TestResourceSubaccountTrustConfiguration(t *testing.T) {
 					Config: hclProvider() + hclResourceSubaccountTrustConfigurationMinimum("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "terraformint.accounts400.ondemand.com"),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "subaccount_id", regexpValidUUID),
-						resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "created_date", regexpValidRFC3999Format),
-						resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "last_modified", regexpValidRFC3999Format),
+						// resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "created_date", regexpValidRFC3999Format),
+						// resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "last_modified", regexpValidRFC3999Format),
 						resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "id", "sap.custom"),
 						resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "identity_provider", "terraformint.accounts400.ondemand.com"),
 						resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "name", "Custom IAS tenant"),
@@ -60,34 +61,9 @@ func TestResourceSubaccountTrustConfiguration(t *testing.T) {
 		})
 	})
 
-	/*
-		t.Run("happy path - minimal configuration 2", func(t *testing.T) {
-			rec := setupVCR(t, "fixtures/resource_subaccount_trust_configuration.minimum2")
-			defer stopQuietly(rec)
-
-			resource.Test(t, resource.TestCase{
-				IsUnitTest:               true,
-				ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
-				Steps: []resource.TestStep{
-					{
-						Config: hclProvider() + hclResourceSubaccountTrustConfigurationMinimum("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "terraformint.accounts400.ondemand.com"),
-						Check: resource.ComposeAggregateTestCheckFunc(
-							resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "subaccount_id", regexpValidUUID),
-							resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "created_date", regexpValidRFC3999Format),
-							resource.TestMatchResourceAttr("btp_subaccount_trust_configuration.uut", "last_modified", regexpValidRFC3999Format),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "id", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "identity_provider", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "name", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "description", "hana-cloud-hana"),
-							resource.TestCheckResourceAttr("btp_subaccount_trust_configuration.uut", "origin", "hana-cloud-hana"),
-						),
-					},
-				},
-			})
-		})
-	*/
 }
 
+/*
 func hclResourceSubaccountTrustConfigurationComplete(resourceName string, subaccountId string, identityProvider string, name string, description string, origin string) string {
 	template := `
 resource "btp_subaccount_trust_configuration" "%s" {
@@ -100,6 +76,7 @@ resource "btp_subaccount_trust_configuration" "%s" {
 
 	return fmt.Sprintf(template, resourceName, subaccountId, identityProvider, name, description, origin)
 }
+*/
 
 func hclResourceSubaccountTrustConfigurationMinimum(resourceName string, subaccountId string, identityProvider string) string {
 	template := `
