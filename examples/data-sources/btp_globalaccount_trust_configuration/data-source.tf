0d97c8fd3638c7b9fcb1d6098432e59f6a946cac
Christian Volk <christian.volk@sap.com>
2023-06-16 14:43:15 +0200

    chore(trust_configuration): update samples (#186)

--- examples/data-sources/btp_globalaccount_trust_configuration/data-source.tf
+++ examples/data-sources/btp_globalaccount_trust_configuration/data-source.tf
@@ -1,3 +1,9 @@
+# default identity provider
 data "btp_globalaccount_trust_configuration" "default" {
   origin = "sap.default"
+}
+
+# custom identity provider
+data "btp_globalaccount_trust_configuration" "custom" {
+  origin = "terraformint-platform"
 }
--- examples/data-sources/btp_subaccount_trust_configuration/data-source.tf
+++ examples/data-sources/btp_subaccount_trust_configuration/data-source.tf
@@ -1,4 +1,11 @@
+# default identity provider
 data "btp_subaccount_trust_configuration" "default" {
   subaccount_id = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
   origin        = "sap.default"
+}
+
+# custom identity provider
+data "btp_subaccount_trust_configuration" "custom" {
+  subaccount_id = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
+  origin        = "terraformint-platform"
 }
