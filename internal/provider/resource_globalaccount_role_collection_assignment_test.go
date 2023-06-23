8d2bd3ebe224f9b4db2bbdd591f640226530e88e
Daniel Kuntze <137066232+kuntzed@users.noreply.github.com>
2023-06-23 14:15:39 +0200

    test: for resource_globalaccount_role_collection_assignment (#217)
    * test for resource_globalaccount_role_collection_assignment
    
    - adding test and fixtures for globalaccount role collection assignment resource
    - adding missing id field required for testing (resource and doc)
    
    * generate docs

---
+++ internal/provider/resource_globalaccount_role_collection_assignment_test.go
@@ -0,0 +1,84 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestResourceGlobalaccountRoleCollectionAssignment(t *testing.T) {
+	t.Parallel()
+	t.Run("happy path - simple role collection assignment", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/resource_globalaccount_role_collection_assignment")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclResourceGlobalaccountRoleCollectionAssignment("uut", "Destination Administrator", "jenny.doe@test.com"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("btp_globalaccount_role_collection_assignment.uut", "role_collection_name", "Destination Administrator"),
+						resource.TestCheckResourceAttr("btp_globalaccount_role_collection_assignment.uut", "user_name", "jenny.doe@test.com"),
+						resource.TestCheckResourceAttr("btp_globalaccount_role_collection_assignment.uut", "origin", "ldap"),
+					),
+				},
+			},
+		})
+	})
+
+	t.Run("happy path - role collection assignment with origin", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/resource_globalaccount_role_collection_assignment_with_origin")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclResourceGlobalaccountRoleCollectionAssignmentWithOrigin("uut", "Destination Administrator", "john.doe@test.com", "terraformint-platform"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("btp_globalaccount_role_collection_assignment.uut", "role_collection_name", "Destination Administrator"),
+						resource.TestCheckResourceAttr("btp_globalaccount_role_collection_assignment.uut", "user_name", "john.doe@test.com"),
+						resource.TestCheckResourceAttr("btp_globalaccount_role_collection_assignment.uut", "origin", "terraformint-platform"),
+					),
+				},
+			},
+		})
+	})
+
+	t.Run("error path - role_collection_name mandatory", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + `resource "btp_globalaccount_role_collection_assignment" "uut" {}`,
+					ExpectError: regexp.MustCompile(`The argument "role_collection_name" is required, but no definition was found.`),
+				},
+			},
+		})
+	})
+}
+
+func hclResourceGlobalaccountRoleCollectionAssignment(resourceName string, roleCollectionName string, userName string) string {
+
+	return fmt.Sprintf(`
+resource "btp_globalaccount_role_collection_assignment" "%s"{
+	role_collection_name = "%s"
+	user_name            = "%s"
+}`, resourceName, roleCollectionName, userName)
+}
+
+func hclResourceGlobalaccountRoleCollectionAssignmentWithOrigin(resourceName string, roleCollectionName string, userName string, origin string) string {
+
+	return fmt.Sprintf(`
+resource "btp_globalaccount_role_collection_assignment" "%s"{
+	role_collection_name = "%s"
+	user_name            = "%s"
+	origin               = "%s"
+}`, resourceName, roleCollectionName, userName, origin)
+}
