a55afb1a16468117e0b8c3f95fa6c6176a8bafda
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-06-28 15:23:08 +0200

    feat(resource): inclusion in test setup

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -139,9 +139,7 @@ func convertFirstLetterToUpperCase(stringToConvert string) (convertedString stri
 func TestProvider_HasResources(t *testing.T) {
 	expectedResources := []string{
 		"btp_directory",
-		/* TODO: switched off for phase 1
-		/"btp_directory_role",
-		*/
+		"btp_directory_role",
 		"btp_directory_role_collection",
 		"btp_directory_role_collection_assignment",
 		"btp_globalaccount_resource_provider",
