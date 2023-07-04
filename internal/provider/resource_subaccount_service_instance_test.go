fc5ae473b054d166ccaedf18e1c7cb85e69c430f
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-07-04 09:39:56 +0200

    feat(resource): add test with parameters

--- internal/provider/resource_subaccount_service_instance_test.go
+++ internal/provider/resource_subaccount_service_instance_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 	"testing"
@@ -8,6 +9,29 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
+
+type testDestinationEntry struct {
+	Name           string `json:"Name"`
+	Type           string `json:"Type"`
+	Url            string `json:"URL"`
+	Authentication string `json:"Authentication"`
+	ProxyType      string `json:"ProxyType"`
+	Description    string `json:"Description"`
+}
+
+type testDestinationSubaccountData struct {
+	ExistingDestinationPolicy string                 `json:"existing_destinations_policy"`
+	Destination               []testDestinationEntry `json:"destinations"`
+}
+
+type testDestinationInitData struct {
+	Subaccount testDestinationSubaccountData `json:"subaccount"`
+}
+
+type testParamsDestination struct {
+	HTML5RuntimeEnabled string                  `json:"HTML5Runtime_enable"`
+	InitData            testDestinationInitData `json:init_data`
+}
 
 func TestResourceSubaccountServiceInstance(t *testing.T) {
 	t.Run("happy path - simple service creation wo parameters", func(t *testing.T) {
@@ -36,6 +60,31 @@ func TestResourceSubaccountServiceInstance(t *testing.T) {
 					ImportStateIdFunc: getServiceInstanceIdForImport("btp_subaccount_service_instance.uut"),
 					ImportState:       true,
 					ImportStateVerify: true,
+				},
+			},
+		})
+	})
+
+	t.Run("happy path - simple service creation with parameters", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/resource_subaccount_service_instance_with_parameters")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclResourceSubaccountServiceInstanceWithParameters("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "tf-test-destintion", "cdf9c103-ef56-43e5-ac1d-4f1c5b15e05c"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestMatchResourceAttr("btp_subaccount_service_instance.uut", "id", regexpValidUUID),
+						resource.TestMatchResourceAttr("btp_subaccount_service_instance.uut", "subaccount_id", regexpValidUUID),
+						resource.TestCheckResourceAttr("btp_subaccount_service_instance.uut", "serviceplan_id", "cdf9c103-ef56-43e5-ac1d-4f1c5b15e05c"),
+						resource.TestMatchResourceAttr("btp_subaccount_service_instance.uut", "created_date", regexpValidRFC3999Format),
+						resource.TestMatchResourceAttr("btp_subaccount_service_instance.uut", "last_modified", regexpValidRFC3999Format),
+						resource.TestCheckResourceAttr("btp_subaccount_service_instance.uut", "usable", "true"),
+						resource.TestCheckResourceAttr("btp_subaccount_service_instance.uut", "name", "tf-test-destintion"),
+						resource.TestCheckResourceAttr("btp_subaccount_service_instance.uut", "platform_id", "service-manager"),
+					),
 				},
 			},
 		})
@@ -106,38 +155,72 @@ func TestResourceSubaccountServiceInstance(t *testing.T) {
 func hclResourceSubaccountServiceInstanceWoParameters(resourceName string, subaccountId string, name string, servicePlanId string) string {
 
 	return fmt.Sprintf(`
-resource "btp_subaccount_service_instance" "%s"{
-    subaccount_id    = "%s"
-	name             = "%s"
-	serviceplan_id   = "%s"
-}`, resourceName, subaccountId, name, servicePlanId)
+		resource "btp_subaccount_service_instance" "%s"{
+		    subaccount_id    = "%s"
+			name             = "%s"
+			serviceplan_id   = "%s"
+		}`, resourceName, subaccountId, name, servicePlanId)
+}
+
+func hclResourceSubaccountServiceInstanceWithParameters(resourceName string, subaccountId string, name string, servicePlanId string) string {
+
+	destinationInitData := testDestinationInitData{
+		Subaccount: testDestinationSubaccountData{
+			ExistingDestinationPolicy: "fail",
+			Destination: []testDestinationEntry{
+				{
+					Name:           "Task_Center_global_settings",
+					Type:           "HTTP",
+					Url:            "http://sap.com",
+					Authentication: "NoAuthentication",
+					ProxyType:      "Internet",
+					Description:    "SAP Task Center Global Settings",
+				},
+			},
+		},
+	}
+
+	destParameters := testParamsDestination{
+		HTML5RuntimeEnabled: "true",
+		InitData:            destinationInitData,
+	}
+
+	destParametersJson, _ := json.Marshal(destParameters)
+
+	return fmt.Sprintf(`
+		resource "btp_subaccount_service_instance" "%s"{
+		    subaccount_id    = "%s"
+			name             = "%s"
+			serviceplan_id   = "%s"
+			parameters       = %q
+		}`, resourceName, subaccountId, name, servicePlanId, string(destParametersJson))
 }
 
 func hclResourceSubaccountServiceInstanceNoSubaccountId(resourceName string, name string, servicePlanId string) string {
 
 	return fmt.Sprintf(`
-resource "btp_subaccount_service_instance" "%s"{
-    name             = "%s"
-	serviceplan_id   = "%s"
-}`, resourceName, name, servicePlanId)
+		resource "btp_subaccount_service_instance" "%s"{
+		    name             = "%s"
+			serviceplan_id   = "%s"
+		}`, resourceName, name, servicePlanId)
 }
 
 func hclResourceSubaccountServiceInstanceNoServicName(resourceName string, subaccountId string, servicePlanId string) string {
 
 	return fmt.Sprintf(`
-resource "btp_subaccount_service_instance" "%s"{
-    subaccount_id    = "%s"
-	serviceplan_id   = "%s"
-}`, resourceName, subaccountId, servicePlanId)
+		resource "btp_subaccount_service_instance" "%s"{
+		    subaccount_id    = "%s"
+			serviceplan_id   = "%s"
+		}`, resourceName, subaccountId, servicePlanId)
 }
 
 func hclResourceSubaccountServiceInstanceNoPlan(resourceName string, subaccountId string, name string) string {
 
 	return fmt.Sprintf(`
-resource "btp_subaccount_service_instance" "%s"{
-    subaccount_id    = "%s"
-	name             = "%s"
-}`, resourceName, subaccountId, name)
+		resource "btp_subaccount_service_instance" "%s"{
+		    subaccount_id    = "%s"
+			name             = "%s"
+		}`, resourceName, subaccountId, name)
 }
 
 func getServiceInstanceIdForImport(resourceName string) resource.ImportStateIdFunc {
