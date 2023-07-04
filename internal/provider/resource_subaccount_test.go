8dbfc45b16a423159427d11ad28983652aa47e98
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-07-04 07:51:07 +0200

    Revert "fix: Add usage to subaccount resource (#266)" (#269)
    This reverts commit 2fa6faf64611bc0b1f4a27eb2ff97a1f2475f772.

--- internal/provider/resource_subaccount_test.go
+++ internal/provider/resource_subaccount_test.go
@@ -61,35 +61,6 @@ func TestResourceSubaccount(t *testing.T) {
 			},
 		})
 	})
-
-	t.Run("happy path - usage", func(t *testing.T) {
-		rec := setupVCR(t, "fixtures/resource_subaccount.usage_set")
-		defer stopQuietly(rec)
-
-		resource.Test(t, resource.TestCase{
-			IsUnitTest:               true,
-			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
-			Steps: []resource.TestStep{
-				{
-					Config: hclProvider() + hclResourceSubaccountForProdUsage("uut", "integration-test-acc-dyn-2", "eu12", "integration-test-acc-dyn-2"),
-					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestMatchResourceAttr("btp_subaccount.uut", "id", regexpValidUUID),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "name", "integration-test-acc-dyn-2"),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "description", ""),
-						resource.TestMatchResourceAttr("btp_subaccount.uut", "parent_id", regexpValidUUID),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "subdomain", "integration-test-acc-dyn-2"),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "created_by", "john.doe@int.test"),
-						resource.TestMatchResourceAttr("btp_subaccount.uut", "created_date", regexpValidRFC3999Format),
-						resource.TestMatchResourceAttr("btp_subaccount.uut", "last_modified", regexpValidRFC3999Format),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "state", "OK"),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "usage", "USED_FOR_PRODUCTION"),
-						resource.TestCheckResourceAttr("btp_subaccount.uut", "beta_enabled", "false"),
-					),
-				},
-			},
-		})
-	})
-
 	t.Run("error path - parent_id not a valid UUID", func(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			IsUnitTest:               true,
@@ -155,18 +126,6 @@ resource "btp_subaccount" "%s" {
     name      = "%s"
     region    = "%s"
     subdomain = "%s"
-}`
-
-	return fmt.Sprintf(template, resourceName, displayName, region, subdomain)
-}
-
-func hclResourceSubaccountForProdUsage(resourceName string, displayName string, region string, subdomain string) string {
-	template := `
-resource "btp_subaccount" "%s" {
-    name      = "%s"
-    region    = "%s"
-    subdomain = "%s"
-	used_for_production = "true"
 }`
 
 	return fmt.Sprintf(template, resourceName, displayName, region, subdomain)
