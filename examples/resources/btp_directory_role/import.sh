edc267c01b1d011e02f000771f101a93f475a488
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-07-05 10:06:19 +0200

    feat(resource): add import to resource_directory_role (#246)

---
+++ examples/resources/btp_directory_role/import.sh
@@ -0,0 +1,3 @@
+# terraform import btp_directory_role.<resource_name> '<directory_id>,<name>,<role_template_name>,<app_id>'
+
+terraform import btp_directory_role.directory_viewer '6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f,Directory Viewer,Directory_Viewer,cis-central!b13'
