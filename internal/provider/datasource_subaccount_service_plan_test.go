c597ed3fa9b35ab9fce8405669ebc5c78d778277
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-07-06 15:16:57 +0200

    feat: datasources for service instances, plans and offerings

---
+++ internal/provider/datasource_subaccount_service_plan_test.go
@@ -0,0 +1,120 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestDataSourceSubaccountServicePlan(t *testing.T) {
+	t.Parallel()
+	t.Run("happy path - service plan by id", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_subaccount_service_plan_by_id")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclDatasourceSubaccountPlanById("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "cdf9c103-ef56-43e5-ac1d-4f1c5b15e05c"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "subaccount_id", "59cd458e-e66e-4b60-b6d8-8f219379f9a5"),
+					),
+				},
+			},
+		})
+	})
+	t.Run("happy path service plan  by name", func(t *testing.T) {
+		rec := setupVCR(t, "fixtures/datasource_subaccount_service_plan_by_name")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			Steps: []resource.TestStep{
+				{
+					Config: hclProvider() + hclDatasourceSubaccountPlanByNameAndOffering("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "lite", "destination"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "subaccount_id", "59cd458e-e66e-4b60-b6d8-8f219379f9a5"),
+					),
+				},
+			},
+		})
+	})
+
+	t.Run("error path - offering name mandatory in case of name", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceSubaccountPlanWoOffering("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "standard"),
+					ExpectError: regexp.MustCompile(`Attribute "offering_name" must be specified when "name" is specified`),
+				},
+			},
+		})
+	})
+
+	t.Run("error path - subaccount id mandatory", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(nil),
+			Steps: []resource.TestStep{
+				{
+					Config:      hclProvider() + hclDatasourceSubaccountPlanWoSubaccount("uut", "lite", "destination"),
+					ExpectError: regexp.MustCompile(`The argument "subaccount_id" is required, but no definition was found.`),
+				},
+			},
+		})
+	})
+
+}
+
+func hclDatasourceSubaccountPlanById(resourceName string, subaccountId string, planId string) string {
+	template := `
+data "btp_subaccount_service_plan" "%s" { 
+     subaccount_id = "%s"
+	 id            = "%s" 
+}`
+
+	return fmt.Sprintf(template, resourceName, subaccountId, planId)
+}
+
+func hclDatasourceSubaccountPlanByNameAndOffering(resourceName string, subaccountId string, planName string, offeringName string) string {
+	template := `
+data "btp_subaccount_service_plan" "%s" {
+    subaccount_id = "%s" 
+    name          = "%s"
+	offering_name = "%s"
+}`
+	return fmt.Sprintf(template, resourceName, subaccountId, planName, offeringName)
+}
+
+func hclDatasourceSubaccountPlanWoIdAndName(resourceName string, subaccountId string) string {
+	template := `
+data "btp_subaccount_service_plan" "%s" {
+    subaccount_id = "%s" 
+}`
+	return fmt.Sprintf(template, resourceName, subaccountId)
+}
+
+func hclDatasourceSubaccountPlanWoOffering(resourceName string, subaccountId string, planName string) string {
+	template := `
+data "btp_subaccount_service_plan" "%s" {
+    subaccount_id = "%s" 
+    name          = "%s"
+}`
+	return fmt.Sprintf(template, resourceName, subaccountId, planName)
+}
+
+func hclDatasourceSubaccountPlanWoSubaccount(resourceName string, planName string, offeringName string) string {
+	template := `
+data "btp_subaccount_service_plan" "%s" {
+    name          = "%s"
+	offering_name = "%s"
+}`
+	return fmt.Sprintf(template, resourceName, planName, offeringName)
+}
