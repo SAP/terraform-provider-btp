0990ddf9683f71f364902205b62536a32e03b238
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-08-24 11:02:48 +0200

    docu: correction of environment instance sample (#386)

--- examples/resources/btp_subaccount_environment_instance/resource.tf
+++ examples/resources/btp_subaccount_environment_instance/resource.tf
@@ -1,5 +1,4 @@
-# creates a cloud foundry environment in a given account and grant the orchestration user admin access to it
-data "btp_whoami" "me" {}
+# creates a cloud foundry environment in a given account
 resource "btp_subaccount_environment_instance" "cloudfoundry" {
   subaccount_id    = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
   name             = "my-cf-environment"
@@ -12,11 +11,5 @@ resource "btp_subaccount_environment_instance" "cloudfoundry" {
   # available environments can be looked up using the btp_subaccount_environments datasource
   parameters = jsonencode({
     instance_name = "my-cf-org-name"
-    users = [
-      {
-        id    = data.btp_whoami.me.id
-        email = data.btp_whoami.me.email
-      }
-    ]
   })
 }
