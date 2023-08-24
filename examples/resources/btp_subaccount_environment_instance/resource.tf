4542d072436f5f08f731e8149929edc44101b067
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-08-24 09:34:58 +0200

    docu: correction of environment instance sample

--- examples/resources/btp_subaccount_environment_instance/resource.tf
+++ examples/resources/btp_subaccount_environment_instance/resource.tf
@@ -12,11 +12,5 @@ resource "btp_subaccount_environment_instance" "cloudfoundry" {
   # available environments can be looked up using the btp_subaccount_environments datasource
   parameters = jsonencode({
     instance_name = "my-cf-org-name"
-    users = [
-      {
-        id    = data.btp_whoami.me.id
-        email = data.btp_whoami.me.email
-      }
-    ]
   })
 }
