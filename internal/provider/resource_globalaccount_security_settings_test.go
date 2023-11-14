1ac43f605e787e29ee52896eb56e3f3aaf785e7f
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-11-14 16:25:17 +0100

    feat: fixture recording for resources

--- internal/provider/resource_globalaccount_security_settings_test.go
+++ internal/provider/resource_globalaccount_security_settings_test.go
@@ -19,13 +19,23 @@ func TestResourceGlobalaccountSecuritySettings(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProviderFor(user) + hclResourceGlobalaccountSecuritySettings("uut", "terraformint.accounts400.ondemand.com", 3601, 3602, true, "[\"domain1.test\",\"domain2.test\"]"),
+					Config: hclProviderFor(user) + hclResourceGlobalaccountSecuritySettings("uut", "terraformint-platform", 4500, 4500, true, "[\"domain1.test\",\"domain2.test\"]"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "access_token_validity", "3601"),
-						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "refresh_token_validity", "3602"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "access_token_validity", "4500"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "refresh_token_validity", "4500"),
 						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "treat_users_with_same_email_as_same_user", "true"),
-						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "custom_email_domains[0]", "domain1.test"),
-						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "custom_email_domains[1]", "domain2.test"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "custom_email_domains.#", "2"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "custom_email_domains.0", "domain1.test"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "custom_email_domains.1", "domain2.test"),
+					),
+				},
+				{
+					Config: hclProviderFor(user) + hclResourceGlobalaccountSecuritySettings("uut", "sap.default", 4500, 4500, false, "[]"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "access_token_validity", "4500"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "refresh_token_validity", "4500"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "treat_users_with_same_email_as_same_user", "false"),
+						resource.TestCheckResourceAttr("btp_globalaccount_security_settings.uut", "custom_email_domains.#", "0"),
 					),
 				},
 			},
--- internal/provider/resource_subaccount_security_settings_test.go
+++ internal/provider/resource_subaccount_security_settings_test.go
@@ -19,14 +19,29 @@ func TestResourceSubaccountSecuritySettings(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProviderFor(user) + hclResourceSubaccountSecuritySettings("uut", "ef23ace8-6ade-4d78-9c1f-8df729548bbf", "terraformint.accounts400.ondemand.com", 3601, 3602, true, "[\"domain1.test\",\"domain2.test\"]"),
+					Config: hclProviderFor(user) + hclResourceSubaccountSecuritySettings("uut", "fc26cc61-ac5e-4c7d-9747-725f32a8994e", "terraformint-platform", 3601, 3602, true, "[\"domain1.test\",\"domain2.test\"]"),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestMatchResourceAttr("btp_subaccount_security_settings.uut", "subaccount_id", regexpValidUUID),
 						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "access_token_validity", "3601"),
 						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "refresh_token_validity", "3602"),
 						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "treat_users_with_same_email_as_same_user", "true"),
-						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains[0]", "domain1.test"),
-						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains[1]", "domain2.test"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "default_identity_provider", "terraformint-platform"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains.#", "2"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains.0", "domain1.test"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains.1", "domain2.test"),
+					),
+				},
+
+				{
+					Config: hclProviderFor(user) + hclResourceSubaccountSecuritySettings("uut", "fc26cc61-ac5e-4c7d-9747-725f32a8994e", "terraformint-platform", 4000, 3602, false, "[\"domain1.test\"]"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestMatchResourceAttr("btp_subaccount_security_settings.uut", "subaccount_id", regexpValidUUID),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "access_token_validity", "4000"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "refresh_token_validity", "3602"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "default_identity_provider", "terraformint-platform"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "treat_users_with_same_email_as_same_user", "false"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains.0", "domain1.test"),
+						resource.TestCheckResourceAttr("btp_subaccount_security_settings.uut", "custom_email_domains.#", "1"),
 					),
 				},
 			},
