87ea85db36d4f74c22ff92f1b6bf82ad1ac9bdb3
ad <andre.dossinger@sap.com>
2023-11-22 11:06:09 +0100

    adding users and refactoring

--- integration/main.tf
+++ integration/main.tf
@@ -1,5 +1,5 @@
 ###
-# Setup of names in accordance to naming convention
+# setup of names in accordance to naming convention
 ###
 
 locals {
@@ -18,7 +18,7 @@ locals {
 }
 
 ###
-# Creation of subaccounts
+# subaccounts
 ###
 
 resource "btp_subaccount" "sa_acc_static" {
@@ -56,7 +56,7 @@ resource "btp_subaccount" "sa_security_settings" {
 }
 
 ###
-# Creation of directories
+# directories
 ###
 
 resource "btp_directory" "dir_entitlements" {
@@ -87,21 +87,40 @@ resource "btp_directory" "dir_se_static" {
 }
 
 ###
-# Global Account Role Collection Assignments
+# directory role collection assignments
 ###
 
-resource "btp_globalaccount_role_collection_assignment" "globalaccount_administrators" {
+resource "btp_directory_role_collection_assignment" "drca_dir_se_static_jenny_directory_viewer" {
+  for_each             = toset(["sap.default", "terraformint-platform"])
+  directory_id         = btp_directory.dir_se_static.id
+  role_collection_name = "Directory Viewer"
+  user_name            = "jenny.doe@test.com"
+  origin               = each.value
+}
+
+###
+# global account role collection assignments
+###
+
+resource "btp_globalaccount_role_collection_assignment" "grca_globalaccount_administrators" {
   for_each             = toset(["BTP Terraform Administrator", "BTP Terraform Developer"])
   role_collection_name = "Global Account Administrator"
   group_name           = each.value
   origin               = "terraform-platform"
 }
 
+resource "btp_globalaccount_role_collection_assignment" "grca_jenny_ga_viewer" {
+  for_each             = toset(["sap.default", "terraformint-platform"])
+  role_collection_name = "Global Account Viewer"
+  user_name            = "jenny.doe@test.com"
+  origin               = each.value
+}
+
 ###
-# Subaccount Role Collection Assignments
+# subaccount role collection assignments
 ###
 
-resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_sa_acc_static" {
+resource "btp_subaccount_role_collection_assignment" "srca_sa_acc_static_subaccount_administrators" {
   subaccount_id        = btp_subaccount.sa_acc_static.id
   for_each             = toset(["BTP Terraform Administrator", "BTP Terraform Developer"])
   role_collection_name = "Subaccount Administrator"
@@ -109,7 +128,7 @@ resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_
   origin               = "terraform-platform"
 }
 
-resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_sa_acc_entitlements_stacked" {
+resource "btp_subaccount_role_collection_assignment" "srca_sa_acc_entitlements_stacked_subaccount_administrators" {
   subaccount_id        = btp_subaccount.sa_acc_entitlements_stacked.id
   for_each             = toset(["BTP Terraform Administrator", "BTP Terraform Developer"])
   role_collection_name = "Subaccount Administrator"
@@ -117,7 +136,7 @@ resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_
   origin               = "terraform-platform"
 }
 
-resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_sa_services_static" {
+resource "btp_subaccount_role_collection_assignment" "srca_sa_services_static_subaccount_administrators" {
   subaccount_id        = btp_subaccount.sa_services_static.id
   for_each             = toset(["BTP Terraform Administrator", "BTP Terraform Developer"])
   role_collection_name = "Subaccount Administrator"
@@ -125,7 +144,7 @@ resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_
   origin               = "terraform-platform"
 }
 
-resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_sa_security_settings" {
+resource "btp_subaccount_role_collection_assignment" "srca_sa_security_settings_subaccount_administrators" {
   subaccount_id        = btp_subaccount.sa_security_settings.id
   for_each             = toset(["BTP Terraform Administrator", "BTP Terraform Developer"])
   role_collection_name = "Subaccount Administrator"
@@ -134,22 +153,22 @@ resource "btp_subaccount_role_collection_assignment" "subaccount_administrators_
 }
 
 ###
-# Directory Entitlements
+# directory entitlements
 ###
 
-resource "btp_directory_entitlement" "entitlement_hana_cloud_dir_entitlements" {
+resource "btp_directory_entitlement" "de_dir_entitlements_hana_cloud" {
   directory_id = btp_directory.dir_entitlements.id
   service_name = "hana-cloud"
   plan_name    = "hana"
 }
 
-resource "btp_directory_entitlement" "entitlement_alert_notification_dir_se_static" {
+resource "btp_directory_entitlement" "de_dir_se_static_alert_notification" {
   directory_id = btp_directory.dir_se_static.id
   service_name = "alert-notification"
   plan_name    = "lite"
 }
 
-resource "btp_directory_entitlement" "entitlement_auditlog_dir_se_static" {
+resource "btp_directory_entitlement" "de_dir_se_static_auditlog" {
   directory_id = btp_directory.dir_se_static.id
   service_name = "auditlog"
   plan_name    = "standard"
@@ -157,10 +176,10 @@ resource "btp_directory_entitlement" "entitlement_auditlog_dir_se_static" {
 }
 
 ###
-# Global Account Resource Pprovider
+# global account resource provider
 ###
 
-resource "btp_globalaccount_resource_provider" "ga_resource_provider_aws" {
+resource "btp_globalaccount_resource_provider" "grp_aws" {
   technical_name = "tf_test_resource_provider"
   display_name   = "Test AWS Resource Provider"
   description    = "Description of the resource provider"
@@ -174,10 +193,10 @@ resource "btp_globalaccount_resource_provider" "ga_resource_provider_aws" {
 }
 
 ###
-# Global Account Trust Configuration
+# global account trust configuration
 ###
 
-resource "btp_globalaccount_trust_configuration" "idp_terraformint" {
+resource "btp_globalaccount_trust_configuration" "gtc_idp_terraformint" {
   identity_provider = "terraformint.accounts400.ondemand.com"
   name              = "terraformint-platform"
   description       = "Custom Platform Identity Provider"
@@ -185,96 +204,96 @@ resource "btp_globalaccount_trust_configuration" "idp_terraformint" {
 }
 
 ###
-# Subaccount Subscriptions
+# subaccount subscriptions
 ###
 
-resource "btp_subaccount_subscription" "subscription_sa_services_static_content_agent_ui" {
+resource "btp_subaccount_subscription" "sas_sa_services_static_content_agent_ui" {
   subaccount_id = btp_subaccount.sa_services_static.id
   app_name      = "content-agent-ui"
   plan_name     = "free"
 }
 
 ###
-# Subaccount Entitlements
+# subaccount entitlements
 ###
 
-resource "btp_subaccount_entitlement" "entitlement_sa_services_static_auditlog_viewer" {
+resource "btp_subaccount_entitlement" "se_sa_services_static_auditlog_viewer" {
   subaccount_id = btp_subaccount.sa_services_static.id
   service_name  = "auditlog-viewer"
   plan_name     = "free"
 }
 
-resource "btp_subaccount_entitlement" "entitlement_sa_services_static_alert_notification" {
+resource "btp_subaccount_entitlement" "se_sa_services_static_alert_notification" {
   subaccount_id = btp_subaccount.sa_services_static.id
   service_name  = "alert-notification"
   plan_name     = "free"
 }
 
-resource "btp_subaccount_entitlement" "entitlement_sa_services_static_iban" {
-  subaccount_id = btp_subaccount.sa_services_static.id
-  service_name  = "ibanservice"
-  plan_name     = "default"
-  amount        = 1
-}
+#resource "btp_subaccount_entitlement" "se_sa_services_static_iban" {
+#  subaccount_id = btp_subaccount.sa_services_static.id
+#  service_name  = "ibanservice"
+#  plan_name     = "default"
+#  amount        = 1
+#}
 
 ###
-# Subaccount Service Instances
+# subaccount service instances
 ###
 
-data "btp_subaccount_service_plan" "service_plan_sa_services_static_alert_notification_free" {
+data "btp_subaccount_service_plan" "ssp_sa_services_static_alert_notification_free" {
   subaccount_id = btp_subaccount.sa_services_static.id
   name          = "free"
   offering_name = "alert-notification"
   depends_on    = [
-    btp_subaccount_entitlement.entitlement_sa_services_static_alert_notification
+    btp_subaccount_entitlement.se_sa_services_static_alert_notification
   ]
 }
 
-resource "btp_subaccount_service_instance" "service_instance_sa_services_static_alert_notification_free" {
+resource "btp_subaccount_service_instance" "ssi_sa_services_static_alert_notification_free" {
   subaccount_id  = btp_subaccount.sa_services_static.id
-  serviceplan_id = data.btp_subaccount_service_plan.service_plan_sa_services_static_alert_notification_free.id
+  serviceplan_id = data.btp_subaccount_service_plan.ssp_sa_services_static_alert_notification_free.id
   name           = "tf-testacc-alertnotification-instance"
 }
 
-data "btp_subaccount_service_plan" "service_plan_sa_services_static_iban_default" {
-  subaccount_id = btp_subaccount.sa_services_static.id
-  name          = "default"
-  offering_name = "ibanservice"
-  depends_on    = [
-    btp_subaccount_entitlement.entitlement_sa_services_static_iban
-  ]
-}
+#data "btp_subaccount_service_plan" "ssp_sa_services_static_iban_default" {
+#  subaccount_id = btp_subaccount.sa_services_static.id
+#  name          = "default"
+#  offering_name = "ibanservice"
+#  depends_on    = [
+#    btp_subaccount_entitlement.se_sa_services_static_iban
+#  ]
+#}
 
-resource "btp_subaccount_service_instance" "service_instance_sa_services_static_iban_default" {
-  subaccount_id  = btp_subaccount.sa_services_static.id
-  serviceplan_id = data.btp_subaccount_service_plan.service_plan_sa_services_static_iban_default.id
-  name           = "tf-testacc-iban-sample"
-  labels         = {
-    org          = [
-      "testvalue"
-    ]
-  }
-}
+#resource "btp_subaccount_service_instance" "ssi_sa_services_static_iban_default" {
+#  subaccount_id  = btp_subaccount.sa_services_static.id
+#  serviceplan_id = data.btp_subaccount_service_plan.ssp_sa_services_static_iban_default.id
+#  name           = "tf-testacc-iban-sample"
+#  labels         = {
+#    org          = [
+#      "testvalue"
+#    ]
+#  }
+#}
 
 ###
-# Subaccount Service Bindings
+# subaccount service bindings
 ###
 
 resource "btp_subaccount_service_binding" "binding_sa_services_static_alert_notification_free_sb_test" {
   subaccount_id       = btp_subaccount.sa_services_static.id
-  service_instance_id = btp_subaccount_service_instance.service_instance_sa_services_static_alert_notification_free.id
+  service_instance_id = btp_subaccount_service_instance.ssi_sa_services_static_alert_notification_free.id
   name                = "test-service-binding"
 }
 
 resource "btp_subaccount_service_binding" "binding_sa_services_static_alert_notification_free_sb_test_two" {
   subaccount_id       = btp_subaccount.sa_services_static.id
-  service_instance_id = btp_subaccount_service_instance.service_instance_sa_services_static_alert_notification_free.id
+  service_instance_id = btp_subaccount_service_instance.ssi_sa_services_static_alert_notification_free.id
   name                = "test-service-binding-two"
 }
 
-resource "btp_subaccount_service_binding" "binding_sa_services_static_iban_default_sb_test" {
-  subaccount_id       = btp_subaccount.sa_services_static.id
-  service_instance_id = btp_subaccount_service_instance.service_instance_sa_services_static_iban_default.id
-  name                = "test-service-binding-iban"
-}
+#resource "btp_subaccount_service_binding" "binding_sa_services_static_iban_default_sb_test" {
+#  subaccount_id       = btp_subaccount.sa_services_static.id
+#  service_instance_id = btp_subaccount_service_instance.ssi_sa_services_static_iban_default.id
+#  name                = "test-service-binding-iban"
+#}
 
