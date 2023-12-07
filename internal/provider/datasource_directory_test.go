f4b15fa021f53dd4c5341087927f903ce2dabbf1
ad <andre.dossinger@sap.com>
2023-12-07 10:57:21 +0100

    test: Removes IDs From Datasource Directory Tests

--- internal/provider/datasource_directory_test.go
+++ internal/provider/datasource_directory_test.go
@@ -22,33 +22,33 @@ func TestDataSourceDirectory(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{ // normal directory
-					Config: hclProviderFor(user) + hclDatasourceDirectory("uut", "5357bda0-8651-4eab-a69d-12d282bc3247"),
+					Config: hclProviderFor(user) + hclDatasourceDirectory("uut", "integration-test-dir-static"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_directory.uut", "id", "5357bda0-8651-4eab-a69d-12d282bc3247"),
+						resource.TestMatchResourceAttr("data.btp_directory.uut", "id", regexpValidUUID),
 						resource.TestCheckResourceAttrSet("data.btp_directory.uut", "created_by"),
-						resource.TestCheckResourceAttr("data.btp_directory.uut", "created_date", "2023-05-16T08:39:33Z"),
+						//resource.TestCheckResourceAttr("data.btp_directory.uut", "created_date", "2023-05-16T08:39:33Z"),
 						resource.TestCheckResourceAttr("data.btp_directory.uut", "description", "Please don't modify. This is used for integration tests."),
 						resource.TestCheckResourceAttr("data.btp_directory.uut", "labels.#", "0"),
 						resource.TestMatchResourceAttr("data.btp_directory.uut", "last_modified", regexpValidRFC3999Format),
 						resource.TestCheckResourceAttr("data.btp_directory.uut", "name", "integration-test-dir-static"),
-						resource.TestCheckResourceAttr("data.btp_directory.uut", "parent_id", "03760ecf-9d89-4189-a92a-1c7efed09298"),
+						//resource.TestCheckResourceAttr("data.btp_directory.uut", "parent_id", "03760ecf-9d89-4189-a92a-1c7efed09298"),
 						resource.TestCheckResourceAttr("data.btp_directory.uut", "state", "OK"),
 						resource.TestCheckResourceAttr("data.btp_directory.uut", "subdomain", ""),
 					),
 				},
 				{ // security enabled directory
-					Config: hclProviderFor(user) + hclDatasourceDirectory("uut_security_enabled", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+					Config: hclProviderFor(user) + hclDatasourceDirectory("uut_security_enabled", "integration-test-dir-se-static"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "id", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+						resource.TestMatchResourceAttr("data.btp_directory.uut_security_enabled", "id", regexpValidUUID),
 						resource.TestCheckResourceAttrSet("data.btp_directory.uut_security_enabled", "created_by"),
-						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "created_date", "2023-05-16T08:46:24Z"),
+						//resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "created_date", "2023-05-16T08:46:24Z"),
 						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "description", "Please don't modify. This is used for integration tests."),
 						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "labels.#", "0"),
 						resource.TestMatchResourceAttr("data.btp_directory.uut_security_enabled", "last_modified", regexpValidRFC3999Format),
 						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "name", "integration-test-dir-se-static"),
-						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "parent_id", "03760ecf-9d89-4189-a92a-1c7efed09298"),
+						//resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "parent_id", "03760ecf-9d89-4189-a92a-1c7efed09298"),
 						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "state", "OK"),
-						resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "subdomain", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
+						//resource.TestCheckResourceAttr("data.btp_directory.uut_security_enabled", "subdomain", "05368777-4934-41e8-9f3c-6ec5f4d564b9"),
 					),
 				},
 			},
@@ -72,7 +72,7 @@ func TestDataSourceDirectory(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(nil),
 			Steps: []resource.TestStep{
 				{
-					Config:      hclDatasourceDirectory("uut", "this-is-not-a-uuid"),
+					Config:      hclDatasourceDirectoryById("uut", "this-is-not-a-uuid"),
 					ExpectError: regexp.MustCompile(`Attribute id value must be a valid UUID, got: this-is-not-a-uuid`),
 				},
 			},
@@ -93,7 +93,7 @@ func TestDataSourceDirectory(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(srv.Client()),
 			Steps: []resource.TestStep{
 				{
-					Config:      hclProviderForCLIServerAt(srv.URL) + hclDatasourceDirectory("uut", "5357bda0-8651-4eab-a69d-12d282bc3247"),
+					Config:      hclProviderForCLIServerAt(srv.URL) + hclDatasourceDirectoryById("uut", "00000000-0000-0000-0000-000000000000"),
 					ExpectError: regexp.MustCompile(`received response with unexpected status \[Status: 404; Correlation ID:\s+[a-f0-9\-]+\]`),
 				},
 			},
@@ -101,6 +101,15 @@ func TestDataSourceDirectory(t *testing.T) {
 	})
 }
 
-func hclDatasourceDirectory(resourceName string, id string) string {
+func hclDatasourceDirectoryById(resourceName string, id string) string {
 	return fmt.Sprintf(`data "btp_directory" "%s" { id = "%s" }`, resourceName, id)
+}
+
+func hclDatasourceDirectory(resourceName string, directoryName string) string {
+	template := `
+data "btp_directories" "all" {}
+data "btp_directory" "%s" {
+    id = [for dir in data.btp_directories.all.values : dir.id if dir.name == "%s"][0]
+}`
+	return fmt.Sprintf(template, resourceName, directoryName)
 }
