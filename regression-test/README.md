17494ccaa38077eb9f8e8bbcde7cce8fbda807da
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-12-11 12:56:48 +0100

    chore: base setup of regression testing

---
+++ regression-test/README.md
@@ -0,0 +1,3 @@
+# About
+
+This directory contains Terraform definitions for setting up a Global Account for regression testing. The config contains the main files for the setup to validate that the new release does not have any regressions.
---
+++ regression-test/variables.tf
@@ -0,0 +1,18 @@
+variable "globalaccount" {
+  type        = string
+  description = "The global account where the project account shall be created in."
+}
+
+
+variable "cli_server" {
+  type        = string
+  description = "The URL of the CLI server"
+  default     = "https://cpcli.cf.eu10.hana.ondemand.com"
+}
+
+
+variable "region" {
+  type        = string
+  description = "The region where the project account shall be created in."
+  default     = "us10"
+}
