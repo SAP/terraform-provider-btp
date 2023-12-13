19fb96578699963c110c1f1ddaa79a665582b2ab
Andre Dossinger <79210154+andre-dossinger@users.noreply.github.com>
2023-12-13 15:37:29 +0000

    test: Removes IDs From Resource Subaccount Service Instance Tests (#608)

--- internal/provider/datasource_subaccount_service_instance_test.go
+++ internal/provider/datasource_subaccount_service_instance_test.go
@@ -20,10 +20,10 @@ func TestDataSourceSubaccountServiceInstance(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProviderFor(user) + hclDatasourceSubaccountServiceInstanceById("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "df532d07-57a7-415e-a261-23a398ef068a"),
+					Config: hclProviderFor(user) + hclDatasourceSubaccountServiceInstanceBySubaccountIdByIdFilteredFromList("uut", "integration-test-services-static", "tf-testacc-alertnotification-instance"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "subaccount_id", "59cd458e-e66e-4b60-b6d8-8f219379f9a5"),
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "id", "df532d07-57a7-415e-a261-23a398ef068a"),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_instance.uut", "subaccount_id", regexpValidUUID),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_instance.uut", "id", regexpValidUUID),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "name", "tf-testacc-alertnotification-instance"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "ready", "true"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "serviceplan_id", "f0aac855-474d-4016-9529-61c062efbc7c"),
@@ -36,6 +36,7 @@ func TestDataSourceSubaccountServiceInstance(t *testing.T) {
 			},
 		})
 	})
+
 	t.Run("happy path - service instance by name", func(t *testing.T) {
 		rec, user := setupVCR(t, "fixtures/datasource_subaccount_service_instance.by_name")
 		defer stopQuietly(rec)
@@ -45,10 +46,10 @@ func TestDataSourceSubaccountServiceInstance(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProviderFor(user) + hclDatasourceSubaccountServiceInstanceByName("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "tf-testacc-alertnotification-instance"),
+					Config: hclProviderFor(user) + hclDatasourceSubaccountServiceInstanceBySubaccountIdByNameFilteredFromList("uut", "integration-test-services-static", "tf-testacc-alertnotification-instance"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "subaccount_id", "59cd458e-e66e-4b60-b6d8-8f219379f9a5"),
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "id", "df532d07-57a7-415e-a261-23a398ef068a"),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_instance.uut", "subaccount_id", regexpValidUUID),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_instance.uut", "id", regexpValidUUID),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "name", "tf-testacc-alertnotification-instance"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "ready", "true"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_instance.uut", "serviceplan_id", "f0aac855-474d-4016-9529-61c062efbc7c"),
@@ -68,7 +69,7 @@ func TestDataSourceSubaccountServiceInstance(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(nil),
 			Steps: []resource.TestStep{
 				{
-					Config:      hclDatasourceSubaccountServiceInstanceIdName("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "tf-testacc-alertnotification-instance"),
+					Config:      hclDatasourceSubaccountServiceInstanceIdName("uut", "00000000-0000-0000-0000-000000000000", "00000000-0000-0000-0000-000000000000", "any-service-instance-name"),
 					ExpectError: regexp.MustCompile(`Error: Invalid Attribute Combination`),
 				},
 			},
@@ -87,24 +88,30 @@ func TestDataSourceSubaccountServiceInstance(t *testing.T) {
 			},
 		})
 	})
+
 }
 
-func hclDatasourceSubaccountServiceInstanceById(resourceName string, subaccountId string, serviceId string) string {
+func hclDatasourceSubaccountServiceInstanceBySubaccountIdByIdFilteredFromList(resourceName string, subaccountName string, serviceInstanceName string) string {
 	template := `
-data "btp_subaccount_service_instance" "%s" {
-     subaccount_id = "%s"
-	 id            = "%s"
+data "btp_subaccounts" "allsas" {}
+data "btp_subaccount_service_instances" "allssis" {
+  subaccount_id = [for sa in data.btp_subaccounts.allsas.values : sa.id if sa.name == "%[2]s"][0]
+}
+data "btp_subaccount_service_instance" "%[1]s" {
+     subaccount_id = [for sa in data.btp_subaccounts.allsas.values : sa.id if sa.name == "%[2]s"][0]
+	 id            = [for ssi in data.btp_subaccount_service_instances.allssis.values : ssi.id if ssi.name == "%[3]s"][0]
 }`
-	return fmt.Sprintf(template, resourceName, subaccountId, serviceId)
+	return fmt.Sprintf(template, resourceName, subaccountName, serviceInstanceName)
 }
 
-func hclDatasourceSubaccountServiceInstanceByName(resourceName string, subaccountId string, serviceName string) string {
+func hclDatasourceSubaccountServiceInstanceBySubaccountIdByNameFilteredFromList(resourceName string, subaccountName string, serviceName string) string {
 	template := `
+data "btp_subaccounts" "all" {}
 data "btp_subaccount_service_instance" "%s" {
-     subaccount_id = "%s"
-	 name            = "%s"
+     subaccount_id = [for sa in data.btp_subaccounts.all.values : sa.id if sa.name == "%s"][0]
+	 name          = "%s"
 }`
-	return fmt.Sprintf(template, resourceName, subaccountId, serviceName)
+	return fmt.Sprintf(template, resourceName, subaccountName, serviceName)
 }
 
 func hclDatasourceSubaccountServiceInstanceIdName(resourceName string, subaccountId string, serviceId string, serviceName string) string {
