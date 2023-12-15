f092a1118eae1e793fec6664212c9f9769d3429d
Andre Dossinger <79210154+andre-dossinger@users.noreply.github.com>
2023-12-15 09:48:43 +0000

    test: Removes IDs From Resource Subaccount Service Plan Tests (#612)

--- internal/provider/datasource_subaccount_service_plan_test.go
+++ internal/provider/datasource_subaccount_service_plan_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestDataSourceSubaccountServicePlan(t *testing.T) {
 	t.Parallel()
+
 	t.Run("happy path - service plan by id", func(t *testing.T) {
 		rec, user := setupVCR(t, "fixtures/datasource_subaccount_service_plan.by_id")
 		defer stopQuietly(rec)
@@ -19,10 +20,10 @@ func TestDataSourceSubaccountServicePlan(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProviderFor(user) + hclDatasourceSubaccountPlanById("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "cdf9c103-ef56-43e5-ac1d-4f1c5b15e05c"),
+					Config: hclProviderFor(user) + hclDatasourceSubaccountPlanByIdBySubaccountIdFromFilteredList("uut", "integration-test-services-static", "lite", "destination"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "subaccount_id", "59cd458e-e66e-4b60-b6d8-8f219379f9a5"),
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "id", "cdf9c103-ef56-43e5-ac1d-4f1c5b15e05c"),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_plan.uut", "subaccount_id", regexpValidUUID),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_plan.uut", "id", regexpValidUUID),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "name", "lite"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "ready", "true"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "catalog_name", "lite"),
@@ -34,6 +35,7 @@ func TestDataSourceSubaccountServicePlan(t *testing.T) {
 			},
 		})
 	})
+
 	t.Run("happy path service plan  by name", func(t *testing.T) {
 		rec, user := setupVCR(t, "fixtures/datasource_subaccount_service_plan.by_name")
 		defer stopQuietly(rec)
@@ -43,10 +45,10 @@ func TestDataSourceSubaccountServicePlan(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
 			Steps: []resource.TestStep{
 				{
-					Config: hclProviderFor(user) + hclDatasourceSubaccountPlanByNameAndOffering("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "lite", "destination"),
+					Config: hclProviderFor(user) + hclDatasourceSubaccountPlanByNameByOfferingBySubaccountIdFromFilteredList("uut", "integration-test-services-static", "lite", "destination"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "subaccount_id", "59cd458e-e66e-4b60-b6d8-8f219379f9a5"),
-						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "id", "cdf9c103-ef56-43e5-ac1d-4f1c5b15e05c"),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_plan.uut", "subaccount_id", regexpValidUUID),
+						resource.TestMatchResourceAttr("data.btp_subaccount_service_plan.uut", "id", regexpValidUUID),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "name", "lite"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "ready", "true"),
 						resource.TestCheckResourceAttr("data.btp_subaccount_service_plan.uut", "catalog_name", "lite"),
@@ -65,7 +67,7 @@ func TestDataSourceSubaccountServicePlan(t *testing.T) {
 			ProtoV6ProviderFactories: getProviders(nil),
 			Steps: []resource.TestStep{
 				{
-					Config:      hclDatasourceSubaccountPlanWoOffering("uut", "59cd458e-e66e-4b60-b6d8-8f219379f9a5", "standard"),
+					Config:      hclDatasourceSubaccountPlanWoOffering("uut", "00000000-0000-0000-0000-000000000000", "standard"),
 					ExpectError: regexp.MustCompile(`Attribute "offering_name" must be specified when "name" is specified`),
 				},
 			},
@@ -87,24 +89,33 @@ func TestDataSourceSubaccountServicePlan(t *testing.T) {
 
 }
 
-func hclDatasourceSubaccountPlanById(resourceName string, subaccountId string, planId string) string {
+func hclDatasourceSubaccountPlanByIdBySubaccountIdFromFilteredList(resourceName string, subaccountName string, planName string, offeringName string) string {
 	template := `
-data "btp_subaccount_service_plan" "%s" {
-     subaccount_id = "%s"
-	 id            = "%s"
+data "btp_subaccounts" "allsas" {}
+data "btp_subaccount_service_plans" "allssps" {
+	subaccount_id = [for sa in data.btp_subaccounts.allsas.values : sa.id if sa.name == "%[2]s"][0]
+}
+data "btp_subaccount_service_offering" "sso" {
+	subaccount_id = [for sa in data.btp_subaccounts.allsas.values : sa.id if sa.name == "%[2]s"][0]
+	name          = "%[4]s"
+}
+data "btp_subaccount_service_plan" "%[1]s" {
+    subaccount_id = [for sa in data.btp_subaccounts.allsas.values : sa.id if sa.name == "%[2]s"][0]
+	id            = [for ssp in data.btp_subaccount_service_plans.allssps.values : ssp.id if ssp.name == "%[3]s" && ssp.serviceoffering_id == data.btp_subaccount_service_offering.sso.id][0]
 }`
 
-	return fmt.Sprintf(template, resourceName, subaccountId, planId)
+	return fmt.Sprintf(template, resourceName, subaccountName, planName, offeringName)
 }
 
-func hclDatasourceSubaccountPlanByNameAndOffering(resourceName string, subaccountId string, planName string, offeringName string) string {
+func hclDatasourceSubaccountPlanByNameByOfferingBySubaccountIdFromFilteredList(resourceName string, subaccountName string, planName string, offeringName string) string {
 	template := `
+data "btp_subaccounts" "all" {}
 data "btp_subaccount_service_plan" "%s" {
-    subaccount_id = "%s"
+    subaccount_id = [for sa in data.btp_subaccounts.all.values : sa.id if sa.name == "%s"][0]
     name          = "%s"
 	offering_name = "%s"
 }`
-	return fmt.Sprintf(template, resourceName, subaccountId, planName, offeringName)
+	return fmt.Sprintf(template, resourceName, subaccountName, planName, offeringName)
 }
 
 func hclDatasourceSubaccountPlanWoOffering(resourceName string, subaccountId string, planName string) string {
