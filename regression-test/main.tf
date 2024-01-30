418ea1e4b0f4de63bdf7c8423efcb9b8099047db
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-01-30 12:58:06 +0100

    chore: update of setup

--- regression-test/main.tf
+++ regression-test/main.tf
@@ -3,18 +3,22 @@
 ###
 
 locals {
-  prefix_integration_test                       = "regression-test-"
-  prefix_integration_test_dir                   = "${local.prefix_integration_test}dir-"
-  prefix_integration_test_account               = "${local.prefix_integration_test}acc-"
-  integration_test_account_static               = "${local.prefix_integration_test_account}static"
-  integration_test_account_entitlements_stacked = "${local.prefix_integration_test_account}entitlements-stacked"
-  integration_test_services_static              = "${local.prefix_integration_test}services-static"
-  integration_test_security_settings            = "${local.prefix_integration_test}security-settings"
-  integration_test_dir_static                   = "${local.prefix_integration_test_dir}static"
-  integration_test_dir_se_static                = "${local.prefix_integration_test_dir}se-static"
-  integration_test_dir_entitlements             = "${local.prefix_integration_test_dir}entitlements"
-  integration_test_dir_entitlements_stacked     = "${local.prefix_integration_test_dir}entitlements-stacked"
-  disclaimer_description                        = "Don't modify. This is used for regression tests."
+  prefix_integration_test                                = "integration-test-"
+  prefix_integration_test_dir                            = "${local.prefix_integration_test}dir-"
+  prefix_integration_test_account                        = "${local.prefix_integration_test}acc-"
+  integration_test_account_static                        = "${local.prefix_integration_test_account}static"
+  integration_test_account_static_extended               = "${local.integration_test_account_static}-${var.subaccount_subdomain_extension}"
+  integration_test_account_entitlements_stacked          = "${local.prefix_integration_test_account}entitlements-stacked"
+  integration_test_account_entitlements_stacked_extended = "${local.integration_test_account_entitlements_stacked}-${var.subaccount_subdomain_extension}"
+  integration_test_services_static                       = "${local.prefix_integration_test}services-static"
+  integration_test_services_static_extended              = "${local.integration_test_services_static}-${var.subaccount_subdomain_extension}"
+  integration_test_security_settings                     = "${local.prefix_integration_test}security-settings"
+  integration_test_security_settings_extended            = "${local.integration_test_security_settings}-${var.subaccount_subdomain_extension}"
+  integration_test_dir_static                            = "${local.prefix_integration_test_dir}static"
+  integration_test_dir_se_static                         = "${local.prefix_integration_test_dir}se-static"
+  integration_test_dir_entitlements                      = "${local.prefix_integration_test_dir}entitlements"
+  integration_test_dir_entitlements_stacked              = "${local.prefix_integration_test_dir}entitlements-stacked"
+  disclaimer_description                                 = "Please don't modify. This is used for integration tests."
 }
 
 ###
@@ -24,9 +28,9 @@ locals {
 resource "btp_subaccount" "sa_acc_static" {
   name        = local.integration_test_account_static
   description = local.disclaimer_description
-  subdomain   = local.integration_test_account_static
+  subdomain   = local.integration_test_account_static_extended
   region      = var.region
-  labels = {
+  labels      = {
     label1 = [
       "label text 1"
     ]
@@ -37,20 +41,20 @@ resource "btp_subaccount" "sa_acc_static" {
 resource "btp_subaccount" "sa_acc_entitlements_stacked" {
   parent_id = btp_directory.dir_entitlements_stacked.id
   name      = local.integration_test_account_entitlements_stacked
-  subdomain = local.integration_test_account_entitlements_stacked
+  subdomain = local.integration_test_account_entitlements_stacked_extended
   region    = var.region
 }
 
 resource "btp_subaccount" "sa_services_static" {
-  name        = local.integration_test_services_static
-  subdomain   = local.integration_test_services_static
-  region      = var.region
-  description = "Subaccount to test:\n- Service Instances\n- Service Bindings\n- App Subscriptions"
+  name         = local.integration_test_services_static
+  subdomain    = local.integration_test_services_static_extended
+  region       = var.region
+  description  = "Subaccount to test:\n- Service Instances\n- Service Bindings\n- App Subscriptions"
 }
 
 resource "btp_subaccount" "sa_security_settings" {
   name      = local.integration_test_security_settings
-  subdomain = local.integration_test_security_settings
+  subdomain = local.integration_test_security_settings_extended
   region    = var.region
 }
 
@@ -77,7 +81,7 @@ resource "btp_directory" "dir_se_static" {
   name        = local.integration_test_dir_se_static
   description = local.disclaimer_description
   features    = ["DEFAULT", "ENTITLEMENTS", "AUTHORIZATIONS"]
-  labels = {
+  labels      = {
     my-label-1 = [
       "Label text 1"
     ]
@@ -96,18 +100,9 @@ resource "btp_directory_role_collection_assignment" "drca_dir_se_static_jenny_di
 }
 
 ###
-# global account role collection assignments
-###
-
-resource "btp_globalaccount_role_collection_assignment" "grca_jenny_ga_viewer" {
-  role_collection_name = "Global Account Viewer"
-  user_name            = "jenny.doe@test.com"
-}
-
-###
 # subaccount role collection assignments
 ###
-resource "btp_subaccount_role_collection_assignment" "srca_sa_acc_static_subaccount_administrators" {
+resource "btp_subaccount_role_collection_assignment" "srca_sa_acc_static_jenny_ga_viewer" {
   subaccount_id        = btp_subaccount.sa_acc_static.id
   role_collection_name = "Subaccount Viewer"
   user_name            = "jenny.doe@test.com"
@@ -122,33 +117,34 @@ resource "btp_directory_entitlement" "de_dir_entitlements_hana_cloud" {
   plan_name    = "hana"
 }
 
+resource "btp_directory_entitlement" "de_dir_se_static_alert_notification" {
+  directory_id = btp_directory.dir_se_static.id
+  service_name = "alert-notification"
+  plan_name    = "lite"
+}
+
+resource "btp_directory_entitlement" "de_dir_se_static_auditlog" {
+  directory_id = btp_directory.dir_se_static.id
+  service_name = "auditlog"
+  plan_name    = "standard"
+  amount       = 1
+}
+
 ###
 # global account resource provider
 ###
-
 resource "btp_globalaccount_resource_provider" "grp_aws" {
-  technical_name = "tf_regtest_resource_provider"
+  technical_name = "tf_test_resource_provider"
   display_name   = "Test AWS Resource Provider"
   description    = "Description of the resource provider"
   provider_type  = "AWS"
-  configuration = jsonencode({
+  configuration  = jsonencode({
     access_key_id     = "AWSACCESSKEY"
     secret_access_key = "AWSSECRETKEY"
     vpc_id            = "vpc-test"
     region            = "eu-central-1"
   })
 }
-
-###
-# global account trust configuration
-###
-
-#resource "btp_globalaccount_trust_configuration" "gtc_idp_terraformint" {
-#  identity_provider = "terraformint.accounts400.ondemand.com"
-#  name              = "terraformint-platform"
-#  description       = "Custom Platform Identity Provider"
-#  origin            = "terraformint-platform"
-#}
 
 ###
 # subaccount subscriptions
@@ -190,7 +186,7 @@ data "btp_subaccount_service_plan" "ssp_sa_services_static_alert_notification_fr
   subaccount_id = btp_subaccount.sa_services_static.id
   name          = "free"
   offering_name = "alert-notification"
-  depends_on = [
+  depends_on    = [
     btp_subaccount_entitlement.se_sa_services_static_alert_notification
   ]
 }
@@ -198,14 +194,14 @@ data "btp_subaccount_service_plan" "ssp_sa_services_static_alert_notification_fr
 resource "btp_subaccount_service_instance" "ssi_sa_services_static_alert_notification_free" {
   subaccount_id  = btp_subaccount.sa_services_static.id
   serviceplan_id = data.btp_subaccount_service_plan.ssp_sa_services_static_alert_notification_free.id
-  name           = "tf-regtest-alertnotification-instance"
+  name           = "tf-testacc-alertnotification-instance"
 }
 
 data "btp_subaccount_service_plan" "ssp_sa_services_static_malware_scanner_default" {
   subaccount_id = btp_subaccount.sa_services_static.id
   name          = "clamav"
   offering_name = "malware-scanner"
-  depends_on = [
+  depends_on    = [
     btp_subaccount_entitlement.se_sa_services_static_malware_scanner
   ]
 }
@@ -213,9 +209,9 @@ data "btp_subaccount_service_plan" "ssp_sa_services_static_malware_scanner_defau
 resource "btp_subaccount_service_instance" "ssi_sa_services_static_malware_scanner_default" {
   subaccount_id  = btp_subaccount.sa_services_static.id
   serviceplan_id = data.btp_subaccount_service_plan.ssp_sa_services_static_malware_scanner_default.id
-  name           = "tf-regtest-malware-scanner-sample"
-  labels = {
-    org = [
+  name           = "tf-testacc-malware-scanner-sample"
+  labels         = {
+    org          = [
       "testvalue"
     ]
   }
@@ -228,11 +224,18 @@ resource "btp_subaccount_service_instance" "ssi_sa_services_static_malware_scann
 resource "btp_subaccount_service_binding" "binding_sa_services_static_alert_notification_free_sb_test" {
   subaccount_id       = btp_subaccount.sa_services_static.id
   service_instance_id = btp_subaccount_service_instance.ssi_sa_services_static_alert_notification_free.id
-  name                = "regtest-service-binding"
+  name                = "test-service-binding"
 }
 
 resource "btp_subaccount_service_binding" "binding_sa_services_static_alert_notification_free_sb_test_two" {
   subaccount_id       = btp_subaccount.sa_services_static.id
   service_instance_id = btp_subaccount_service_instance.ssi_sa_services_static_alert_notification_free.id
-  name                = "regtest-service-binding-two"
+  name                = "test-service-binding-two"
 }
+
+resource "btp_subaccount_service_binding" "binding_sa_services_static_malware_scanner_default_sb_test" {
+  subaccount_id       = btp_subaccount.sa_services_static.id
+  service_instance_id = btp_subaccount_service_instance.ssi_sa_services_static_malware_scanner_default.id
+  name                = "test-service-binding-malware-scanner"
+}
+
--- regression-test/variables.tf
+++ regression-test/variables.tf
@@ -21,3 +21,10 @@ variable "region" {
   description = "The region where the project account shall be created in."
   default     = "us10"
 }
+
+variable "subaccount_subdomain_extension" {
+  type        = string
+  description = "Subaccount subdomains are required to be unique across landscapes. Define a custom extension to avoid subdomain collision if operating multiple integration Global Accounts in the same landscape."
+  default     = "main"
+}
+
