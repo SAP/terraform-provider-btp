d3aacf85bf255f247d5bbec136e9a3a821a3e0ff
I584814 <diya.dhandapani.shanmugam@sap.com>
2024-02-13 08:07:23 +0000

    new subaccount and directory types

---
+++ internal/provider/type_subaccountHierarchy.go
@@ -0,0 +1,24 @@
+package provider
+
+import "github.com/hashicorp/terraform-plugin-framework/types"
+
+type subaccountHierarchyType struct {
+	ID             types.String `tfsdk:"id"`
+	CreatedBy      types.String `tfsdk:"created_by"`
+	CreatedDate    types.String `tfsdk:"created_date"`
+	ModifiedDate   types.String	`tfdsk:"modified_date"`
+	Name           types.String `tfsdk:"name"`
+	ParentID       types.String `tfsdk:"parent_id"`
+	ParentName	   types.String `tfsdk:"parent_name"`
+	ParentType     types.String	`tfsdk:"parent_type"`
+	Region         types.String `tfsdk:"region"`
+	State          types.String `tfsdk:"state"`
+	Subdomain      types.String `tfsdk:"subdomain"`
+	Type		   types.String	`tfsdk:"type"`
+
+	// Description    types.String `tfsdk:"description"`
+	// Labels         types.Map    `tfsdk:"labels"`
+	// LastModified   types.String `tfsdk:"last_modified"`
+	// ParentFeatures types.Set    `tfsdk:"parent_features"`
+	// Usage          types.String `tfsdk:"usage"`
+}
