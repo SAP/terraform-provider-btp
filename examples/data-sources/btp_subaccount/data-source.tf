e3b675269b22abae3c422b4cb135602083726b1b
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-03-25 20:15:26 +0100

    docs: add example for subaccount data source (#753)

--- examples/data-sources/btp_subaccount/data-source.tf
+++ examples/data-sources/btp_subaccount/data-source.tf
@@ -1,3 +1,10 @@
-data "btp_subaccount" "my_account" {
+# Read a subaccount by ID
+data "btp_subaccount" "my_account_byid" {
   id = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
+}
+
+# Read a subaccount by region and subdomain
+data "btp_subaccount" "my_account-bysubdomain" {
+  region    = "eu10" 
+	subdomain = "my-subaccount-subdomain"
 }
