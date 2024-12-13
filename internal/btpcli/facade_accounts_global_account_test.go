8b8df2279bb90f76ef59ccd6d87a5785067ddab3
Sarthak Goyal <61136876+sp1goyal@users.noreply.github.com>
2024-12-13 13:58:52 +0530

    test: Improve Sonar Tests Coverage (#975)
    * Update clienttypes_test
    
    Added testcase for NewBrowserLoginRequest
    
    * Update facade_accounts_entitlement_test
    
    Added test functions for GetAssignedBySubaccount and GetEntitledByDirectory
    
    * Update facade_accounts_global_account_test
    
    Added test function for GetWithHierarchy
    
    * Update datasource_subaccount_service_plan_test
    
    Added error testcase for service plan with offering id or offering name
    
    * Update provider_test
    
    Added error testcase for invalid client server url
    
    * Fixture for datasource_subaccount_service_plan error
    
    * Update datasource_subaccount_environment_instances_test
    
    Added one new cf environment instance to the subaccount and re-recorded the test
    
    * Added datasource_subaccount_environment_instance_test
    
    Added a new cf environment instance to the subaccount and recorded new test fixtures
    
    ---------
    
    Co-authored-by: Sarthak Goyal <sarthak.goyal01@sap.com>

--- internal/btpcli/facade_accounts_global_account_test.go
+++ internal/btpcli/facade_accounts_global_account_test.go
@@ -30,3 +30,27 @@ func TestAccountsGlobalAccountFacade_Get(t *testing.T) {
 		}
 	})
 }
+
+func TestAccountsGlobalAccountFacade_GetWithHierarchy(t *testing.T) {
+	command := "accounts/global-account"
+
+	t.Run("constructs the CLI params correctly", func(t *testing.T) {
+		var srvCalled bool
+
+		uut, srv := prepareClientFacadeForTest(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			srvCalled = true
+
+			assertCall(t, r, command, ActionGet, map[string]string{
+				"globalAccount": "795b53bb-a3f0-4769-adf0-26173282a975",
+				"showHierarchy": "true",
+			})
+		}))
+		defer srv.Close()
+
+		_, res, err := uut.Accounts.GlobalAccount.GetWithHierarchy(context.TODO())
+
+		if assert.True(t, srvCalled) && assert.NoError(t, err) {
+			assert.Equal(t, 200, res.StatusCode)
+		}
+	})
+}
