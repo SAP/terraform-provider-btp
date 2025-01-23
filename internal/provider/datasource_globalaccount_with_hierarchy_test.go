b0f15f691fd00301b3bd6a9b675ebbcb87e5f8d9
Sarthak Goyal <sarthak.goyal01@sap.com>
2025-01-23 15:05:56 +0530

    Test fixes and fixtures for globalaccount datasources

--- internal/provider/datasource_globalaccount_with_hierarchy_test.go
+++ internal/provider/datasource_globalaccount_with_hierarchy_test.go
@@ -21,11 +21,11 @@ func TestDataSourceGlobalaccountWithHierarchy(t *testing.T) {
 					Config: hclProviderFor(user) + hclDatasourceGlobalaccountWithHierarchy("globalaccount_canary"),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						//directory: integration-test-dir-entitlements 's parent -->  terraform-integration-canary
-						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "directories.2.parent_name", "terraform-integration-canary"),
+						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "directories.4.parent_name", "terraform-integration-canary"),
 						//directory: integration-test-dir-entitlements-stacked 's parent --> integration-test-dir-entitlements
-						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "directories.2.directories.0.parent_name", "integration-test-dir-entitlements"),
+						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "directories.4.directories.0.parent_name", "integration-test-dir-entitlements"),
 						//subaccount: integration-test-acc-entitlements-stacked 's parent --> integration-test-dir-entitlements-stacked
-						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "directories.2.directories.0.subaccounts.0.parent_name", "integration-test-dir-entitlements-stacked"),
+						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "directories.4.directories.0.subaccounts.0.parent_name", "integration-test-dir-entitlements-stacked"),
 						//subaccount: integration-test-acc-static 's parent --> terraform-integration-canary
 						resource.TestCheckResourceAttr("data.btp_globalaccount_with_hierarchy.globalaccount_canary", "subaccounts.0.parent_name", "terraform-integration-canary"),
 					),
