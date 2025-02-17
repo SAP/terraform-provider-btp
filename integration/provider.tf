d2c252fa00bf2406f016bcbe304b7cd55574378d
Christian Lechner <lechnerc77@users.noreply.github.com>
2025-02-17 08:57:42 +0100

    chore: prep release 1.10.0 (#1023)
    Co-authored-by: Christian Lechner <22294087+lechnerc77@users.noreply.github.com>

--- integration/provider.tf
+++ integration/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     btp = {
       source  = "SAP/btp"
-      version = "1.9.0"
+      version = "1.10.0"
     }
   }
 }
@@ -11,4 +11,3 @@ terraform {
 provider "btp" {
   cli_server_url = "https://canary.cli.btp.int.sap"
 }
-
