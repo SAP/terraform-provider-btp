00dccb3e435a6b9d894531a56ef764e02db5ec84
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2025-08-28 14:46:22 +0200

    feat: adjust internal types for subaccount datasources

--- btp/provider/type_subaccount.go
+++ btp/provider/type_subaccount.go
@@ -16,6 +16,23 @@ const AuthorizationFeature = "AUTHORIZATIONS"
 
 var subdomainRegex = regexp.MustCompile("^[a-z0-9](?:[a-z0-9|-]{0,61}[a-z0-9])?$")
 
+type subaccountDataSourceType struct {
+	ID             types.String `tfsdk:"id"`
+	BetaEnabled    types.Bool   `tfsdk:"beta_enabled"`
+	CreatedBy      types.String `tfsdk:"created_by"`
+	CreatedDate    types.String `tfsdk:"created_date"`
+	Description    types.String `tfsdk:"description"`
+	Labels         types.Map    `tfsdk:"labels"`
+	LastModified   types.String `tfsdk:"last_modified"`
+	Name           types.String `tfsdk:"name"`
+	ParentID       types.String `tfsdk:"parent_id"`
+	ParentFeatures types.Set    `tfsdk:"parent_features"`
+	Region         types.String `tfsdk:"region"`
+	State          types.String `tfsdk:"state"`
+	Subdomain      types.String `tfsdk:"subdomain"`
+	Usage          types.String `tfsdk:"usage"`
+}
+
 type subaccountType struct {
 	ID                  types.String `tfsdk:"id"`
 	BetaEnabled         types.Bool   `tfsdk:"beta_enabled"`
@@ -36,6 +53,33 @@ type subaccountType struct {
 
 func subaccountValueFrom(ctx context.Context, value cis.SubaccountResponseObject) (subaccountType, diag.Diagnostics) {
 	subaccount := subaccountType{
+		ID:           types.StringValue(value.Guid),
+		BetaEnabled:  types.BoolValue(value.BetaEnabled),
+		CreatedBy:    types.StringValue(value.CreatedBy),
+		CreatedDate:  timeToValue(value.CreatedDate.Time()),
+		Description:  types.StringValue(value.Description),
+		LastModified: timeToValue(value.ModifiedDate.Time()),
+		Name:         types.StringValue(value.DisplayName),
+		ParentID:     types.StringValue(value.ParentGUID),
+		Region:       types.StringValue(value.Region),
+		State:        types.StringValue(value.State),
+		Subdomain:    types.StringValue(value.Subdomain),
+		Usage:        types.StringValue(value.UsedForProduction),
+	}
+
+	var diags, diagnostics diag.Diagnostics
+
+	subaccount.Labels, diags = types.MapValueFrom(ctx, types.SetType{ElemType: types.StringType}, value.Labels)
+	diagnostics.Append(diags...)
+
+	subaccount.ParentFeatures, diags = types.SetValueFrom(ctx, types.StringType, value.ParentFeatures)
+	diagnostics.Append(diags...)
+
+	return subaccount, diagnostics
+}
+
+func subaccountDataSourceValueFrom(ctx context.Context, value cis.SubaccountResponseObject) (subaccountDataSourceType, diag.Diagnostics) {
+	subaccount := subaccountDataSourceType{
 		ID:           types.StringValue(value.Guid),
 		BetaEnabled:  types.BoolValue(value.BetaEnabled),
 		CreatedBy:    types.StringValue(value.CreatedBy),
