f2da550261c35e145b67b2a3dc12cf604e091258
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2025-12-08 11:33:22 +0100

    fix: terraform fmt

--- examples/data-sources/btp_subaccount_destinations/data-source.tf
+++ examples/data-sources/btp_subaccount_destinations/data-source.tf
@@ -1,12 +1,12 @@
 # Read all destination for a given subaccount
 data "btp_subaccount_destinations" "dest_by_subacount" {
-  subaccount_id       = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
+  subaccount_id = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
 }
 
 # Read all destination for a given subaccount with names only
 data "btp_subaccount_destinations" "dest_by_subacount_names_only" {
-  subaccount_id       = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
-  names_only          = true
+  subaccount_id = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
+  names_only    = true
 }
 
 # Read all destination for a given subaccount and service instance
