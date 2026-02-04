d65bbca919ead79dbd8a8ebe657971cae94c8730
C5410102 <ravi.yadav03@sap.com>
2026-02-04 05:35:22 +0000

    docs: add documentation for role collection data sources

---
+++ examples/data-sources/btp_subaccount_role_collection_role/data-source.tf
@@ -0,0 +1,5 @@
+data "btp_subaccount_role_collection_role" "Subaccount_Viewer" {
+  subaccount_id = "1ecb7280-c7d4-4db6-b7da-7af3cdb13505"
+  name          = "my_collection"
+  role_name     = "Subaccount Viewer"
+}
---
+++ examples/data-sources/btp_subaccount_role_collection_roles/data-source.tf
@@ -0,0 +1,4 @@
+data "btp_subaccount_role_collection_roles" "all_roles" {
+  subaccount_id = "1ecb7280-c7d4-4db6-b7da-7af3cdb13505"
+  name          = "my_collection"
+}
