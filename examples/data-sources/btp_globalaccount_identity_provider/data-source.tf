b2a1f998d03dce47bd8f8425d253d072993b926f
C5410102 <ravi.yadav03@sap.com>
2026-02-10 06:05:14 +0000

    docs: add documentation and examples for identity provider data sources

---
+++ examples/data-sources/btp_globalaccount_identity_provider/data-source.tf
@@ -0,0 +1,3 @@
+data "btp_globalaccount_identity_provider" "single_global_idp" {
+  host= "<host>"
+}
---
+++ examples/data-sources/btp_subaccount_identity_provider/data-source.tf
@@ -0,0 +1,5 @@
+
+data "btp_subaccount_identity_provider" "single_idp" {
+  subaccount_id = "<subaccount_id>"
+  host= "<host>"
+}
