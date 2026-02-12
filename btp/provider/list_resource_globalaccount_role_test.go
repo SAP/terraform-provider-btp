ba21b7f0d2ae0a63b3000ad31f00b41a0d98a815
C5410102 <ravi.yadav03@sap.com>
2026-02-12 05:33:49 +0000

    feat: add list resource functionality for global account roles

---
+++ btp/provider/list_resource_globalaccount_role_test.go
@@ -0,0 +1,106 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck/queryfilter"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+)
+
+func TestGlobalaccountRoleListResource(t *testing.T) {
+	t.Parallel()
+
+	t.Run("happy path", func(t *testing.T) {
+		rec, user := setupVCR(t, "fixtures/list_resource_globalaccount_role")
+		defer stopQuietly(rec)
+
+		resource.Test(t, resource.TestCase{
+			IsUnitTest:               true,
+			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+				tfversion.SkipBelow(tfversion.Version1_14_0),
+			},
+			Steps: []resource.TestStep{
+				{
+					Query:  true,
+					Config: hclProviderFor(user) + listGlobalAccountRoleQueryConfig("roles_list", "btp"),
+
+					QueryResultChecks: []querycheck.QueryResultCheck{
+						querycheck.ExpectLength("btp_globalaccount_role.roles_list", 14),
+
+						querycheck.ExpectIdentity(
+							"btp_globalaccount_role.roles_list",
+							map[string]knownvalue.Check{
+								"name":               knownvalue.StringExact("Auditlog_Auditor"),
+								"role_template_name": knownvalue.StringExact("Auditlog_Auditor"),
+								"app_id":             knownvalue.StringExact("auditlog-management!b3034"),
+							},
+						),
+					},
+				},
+				{
+					// List Query with include_resource = true
+					Query:  true,
+					Config: hclProviderFor(user) + listGlobalAccountRoleQueryConfigWithIncludeResource("roles_list", "btp"),
+
+					QueryResultChecks: []querycheck.QueryResultCheck{
+						querycheck.ExpectLength("btp_globalaccount_role.roles_list", 14),
+
+						// Verify full resource data is populated
+						querycheck.ExpectResourceKnownValues(
+							"btp_globalaccount_role.roles_list",
+							queryfilter.ByResourceIdentity(map[string]knownvalue.Check{
+								"name":               knownvalue.StringExact("Auditlog_Auditor"),
+								"role_template_name": knownvalue.StringExact("Auditlog_Auditor"),
+								"app_id":             knownvalue.StringExact("auditlog-management!b3034"),
+							}),
+							[]querycheck.KnownValueCheck{
+								{
+									Path:       tfjsonpath.New("description"),
+									KnownValue: knownvalue.StringExact("Read access to audit logs"),
+								},
+								{
+									Path:       tfjsonpath.New("role_template_name"),
+									KnownValue: knownvalue.StringExact("Auditlog_Auditor"),
+								},
+								{
+									Path:       tfjsonpath.New("app_id"),
+									KnownValue: knownvalue.StringExact("auditlog-management!b3034"),
+								},
+								{
+									Path:       tfjsonpath.New("name"),
+									KnownValue: knownvalue.StringExact("Auditlog_Auditor"),
+								},
+								{
+									Path:       tfjsonpath.New("read_only"),
+									KnownValue: knownvalue.Bool(true),
+								},
+							},
+						),
+					},
+				},
+			},
+		})
+	})
+
+}
+
+func listGlobalAccountRoleQueryConfig(label, providerName string) string {
+	return fmt.Sprintf(`
+list "btp_globalaccount_role" "%s" {
+  provider = "%s"
+}`, label, providerName)
+}
+
+func listGlobalAccountRoleQueryConfigWithIncludeResource(label, providerName string) string {
+	return fmt.Sprintf(`
+list "btp_globalaccount_role" "%s" {
+  provider = "%s"
+  include_resource = true
+}`, label, providerName)
+}
