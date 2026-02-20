09a43016a00c9260b3a3a8dc235637e672cd251e
C5410102 <ravi.yadav03@sap.com>
2026-02-20 05:05:42 +0000

    refactor: fix failing test cases

--- btp/provider/list_resource_subaccount_service_broker_test.go
+++ btp/provider/list_resource_subaccount_service_broker_test.go
@@ -133,6 +133,9 @@ func TestSubaccountServiceBrokerListResource(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			IsUnitTest:               true,
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+				tfversion.SkipBelow(tfversion.Version1_14_0),
+			},
 			Steps: []resource.TestStep{
 				{
 					Query:  true,
--- btp/provider/list_resource_subaccount_service_instance_test.go
+++ btp/provider/list_resource_subaccount_service_instance_test.go
@@ -142,6 +142,9 @@ func TestSubaccountServiceInstanceListResource(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			IsUnitTest:               true,
 			ProtoV6ProviderFactories: getProviders(rec.GetDefaultClient()),
+			TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+				tfversion.SkipBelow(tfversion.Version1_14_0),
+			},
 			Steps: []resource.TestStep{
 				{
 					Query:  true,
