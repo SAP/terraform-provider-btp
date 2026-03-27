60f642580484aed784fd751836c04a53cf866086
Christian Lechner <christian.lechner@sap.com>
2026-03-27 10:42:10 +0100

    fix: date format for subscriptions

--- internal/btpcli/types/saas_manager_service/time.go
+++ internal/btpcli/types/saas_manager_service/time.go
@@ -16,6 +16,10 @@ func (t *Time) UnmarshalJSON(b []byte) error {
 
 	layout := "Jan 2, 2006, 3:04:05 PM"
 
+	// Normalize narrow no-break space (U+202F) to a regular space.
+	// Some locales use it as the AM/PM separator instead of a regular space.
+	s = strings.ReplaceAll(s, "\u202f", " ")
+
 	timeString, err := time.Parse(layout, s)
 	if err != nil {
 		return err
