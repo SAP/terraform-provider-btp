187cd4b4e9606229a65fd1bc8cc78d37becc48c4
Sarthak Goyal <sarthak.goyal01@sap.com>
2026-04-09 15:54:26 +0530

    Refactor and new implementation for integration tests for terraform provider

---
+++ fixture-test-setup/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "1.21.3"
+    }
+  }
+}
+
+# Configure the BTP Provider
+provider "btp" {
+  cli_server_url = "https://canary.cli.btp.int.sap"
+}
---
+++ tests/integration-test/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+    }
+  }
+}
+
+# Configure the BTP Provider
+provider "btp" {
+  globalaccount  = var.globalaccount
+  cli_server_url = var.cli_server
+}
