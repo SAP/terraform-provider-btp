c2a020aaf77930fe1f48abcd497d8d1c8a6dac2e
Christian Lechner <lechnerc77@users.noreply.github.com>
2026-06-30 14:34:06 +0200

    fix: refresh trust during update (#1605)

--- internal/btpcli/facade_security_trust.go
+++ internal/btpcli/facade_security_trust.go
@@ -100,7 +100,7 @@ func (f *securityTrustFacade) UpdateByGlobalAccount(ctx context.Context, args Tr
 	}
 
 	params["globalAccount"] = f.cliClient.GetGlobalAccountSubdomain()
-	params["refreshTrust"] = "true"
+	// refreshTrust is intentionally not set on update
 
 	return doExecute[xsuaa_trust.TrustConfigurationResponseObject](f.cliClient, ctx, NewUpdateRequest(f.getCommand(), params))
 }
@@ -113,7 +113,7 @@ func (f *securityTrustFacade) UpdateBySubaccount(ctx context.Context, subaccount
 	}
 
 	params["subaccount"] = subaccountId
-	params["refreshTrust"] = "true"
+	// refreshTrust is intentionally not set on update
 
 	return doExecute[xsuaa_trust.TrustConfigurationResponseObject](f.cliClient, ctx, NewUpdateRequest(f.getCommand(), params))
 }
