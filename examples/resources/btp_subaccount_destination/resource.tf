7609483b698e1ed2d17bdcaa95538fb86ab0f7d1
Christian Lechner <christian.lechner@sap.com>
2026-09-14 15:23:23 +0200

    docs: fix OAUth2 example for destination

--- examples/resources/btp_subaccount_destination/resource.tf
+++ examples/resources/btp_subaccount_destination/resource.tf
@@ -50,10 +50,10 @@ resource "btp_subaccount_destination" "destination-with-additional-configuration
   description    = "resource"
   subaccount_id  = "6aa64c2f-38c1-49a9-b2e8-cf9fea769b7f"
   additional_configuration = jsonencode({
-    ClientID            = "<clientid>"
-    ClientSecret        = "<clientsecret>"
-    TokenServiceURL     = "https://tokenurl"
-    TokenServiceURLType = "dedicated"
+    clientID            = "<clientid>"
+    clientSecret        = "<clientsecret>"
+    tokenServiceURL     = "https://tokenurl"
+    tokenServiceURLType = "Dedicated"
   })
 }
 
